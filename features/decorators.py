dd7a732ab9b31efc9d6732e976cbed4e05595ae8
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-01-15 11:57:02 +0300

    implemented for class-based registering

--- features/decorators.py
+++ features/decorators.py
@@ -1,4 +1,5 @@
 from . import utils
+from .storage import FeatureConstructor
 from IPython.display import display
 from glob import glob
 import os
@@ -11,35 +12,15 @@ def test(function):
     
     return new_function
 
-# not implemented "no_cache_default" parameter
-# TODO: rewrite as a class-based decorator: 
-# https://stackoverflow.com/questions/20093811/how-do-i-change-the-representation-of-a-python-function
-# return something like <feature constructor "make_such_features">
-# it is needed to implement a list of feature constructors, which can be used to share features between several notebooks
-# e.g. 
-# @property
-# def feature_constructors(self):
-#     load_smth()
-# kts.feature_constructors -> [feature_1, feature_2] instead of [<function "0xfoo">, <function "0xbar">]
+
 def register(function, no_cache_default=False):
     if utils.is_cached_src(function) and not utils.matches_cache(function):
         raise NameError(f"A function with the same name is already registered:\n{utils.load_src_func(function)()}")
     utils.cache_src(function)
 
-    def new_function(df, no_cache=no_cache_default):
-        if no_cache:
-            return function(df)
-        if utils.is_cached(function, df):
-            return utils.load_cached(function, df)
-        else:
-            result = function(df)
-            utils.cache(function, df, result)
-            return result
-
-    new_function.__name__ = function.__name__
-    new_function.source = utils.load_src_func(function)
-#     print(new_function.source())
-    return new_function
+    functor = FeatureConstructor(function, no_cache_default)
+    utils.cache_fc(functor)
+    return functor
 
 def deregister(*args, **kwargs):
 #     print(args, kwargs)
