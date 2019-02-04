f9523cc2230797677e3c2dabac7c89b6645ee606
rvg77 <gorb.roman1999@gmail.com>
2019-02-04 19:41:41 +0300

    config edited

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import numpy as np
+import mprop
+
 seed = 31337
 np.random.seed(seed)
 seeds = np.random.randint(100, size=10)
@@ -6,3 +8,21 @@ seeds = np.random.randint(100, size=10)
 storage_path = '../storage/'
 index_prefix = "__kts__index_"
 test_call = 0
+
+
+@property
+def feature_path(config):
+    return storage_path + 'features/'
+
+
+@property
+def info_path(config):
+    return storage_path + 'info/'
+
+
+@property
+def souce_path(config):
+    return storage_path + 'sources'
+
+
+mprop.init()
