ba7dab0f3d81824c38f4abf7b3f4d2b41062605b
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-05 17:27:00 +0300

    added experiment registration during every validation, significantly changed Validator() interface: it is initialized with splitter and metric, scores (model, fs, target)

--- validation/validator.py
+++ validation/validator.py
@@ -1,29 +1,43 @@
 import numpy as np
 
 from .. import config
+from .experiment import experiment_list, Experiment
 from copy import deepcopy
 
 class Validator:
-    def __init__(self, featureset, target, splitter, metric):
-        self.fs = featureset
-        self.target = target
+    def __init__(self, splitter, metric):
         self.splitter = splitter
         self.metric = metric
         
-    def score(self, model):
+    def score(self, model, featureset, target):
         models = []
         scores = []
-        oofs = np.zeros_like(self.target)
-        weights = np.zeros_like(self.target)
+        oofs = np.zeros_like(target)
+        weights = np.zeros_like(target)
         for spl in self.splitter.split:
             idx_train = spl['train']
             idx_test = spl['test']
             c_model = deepcopy(model)
-            c_model.fit(self.fs[idx_train], self.target[idx_train])
-            pred = c_model.predict(self.fs[idx_test])
+            c_model.fit(featureset[idx_train], target[idx_train])
+            pred = c_model.predict(featureset[idx_test])
             oofs[idx_test] = (weights[idx_test] * oofs[idx_test] + pred) / (weights[idx_test] + 1)
             weights[idx_test] += 1
-            score = self.metric(self.target[idx_test], pred)
-            models.append(model)
+            score = self.metric(target[idx_test], pred)
+            models.append(c_model)
             scores.append(score)
-        return np.mean(scores)
+        model_name = f"ens_{models[0].__name__}_x{len(models)}"
+        final_ensemble = models[0]
+        for model in models[1:]:
+            final_ensemble = final_ensemble + model
+        final_ensemble = final_ensemble / len(models)
+        final_ensemble.__name__ = model_name
+        score = np.mean(scores)
+        experiment_list.register(
+            Experiment(
+                final_ensemble,
+                featureset,
+                oofs,
+                score
+            )
+        )
+        return score
