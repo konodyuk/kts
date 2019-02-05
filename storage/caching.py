171972944e061ba1f66625118b71cbd28a0cbceb
rvg77 <gorb.roman1999@gmail.com>
2019-02-05 17:36:43 +0300

    added time support

--- storage/caching.py
+++ storage/caching.py
@@ -1,5 +1,6 @@
 from . import cache_utils
 from .. import config
+import datetime
 from glob import glob
 import os
 
@@ -11,6 +12,7 @@ class Cache:
 
     def __init__(self):
         self.memory = dict()
+        self.last_used = dict()
 
     def cache_df(self, df, name):
         """
@@ -21,23 +23,8 @@ class Cache:
         """
         dict_name = name + '_df'
         self.memory[dict_name] = df
+        self.last_used[dict_name] = datetime.datetime.now()
         cache_utils.save_df(df, cache_utils.get_path_df(name))
-
-    def load_df(self, name):
-        """
-        Loads dataframe from cache
-        :param name: name of object
-        :return: dataframe with given name
-        """
-        dict_name = name + '_df'
-        if dict_name in self.memory:
-            return self.memory[dict_name]
-        elif os.path.exists(cache_utils.get_path_df(name)):
-            tmp = cache_utils.load_df(cache_utils.get_path_df(name))
-            self.memory[dict_name] = tmp
-            return tmp
-        else:
-            raise KeyError("No such df in cache")
 
     def is_cached_df(self, name):
         """
@@ -47,6 +34,24 @@ class Cache:
         """
         dict_name = name + '_df'
         return dict_name in self.memory or os.path.exists(cache_utils.get_path_df(name))
+
+    def load_df(self, name):
+        """
+        Loads dataframe from cache
+        :param name: name of object
+        :return: dataframe with given name
+        """
+        if not self.is_cached_df(name):
+            raise KeyError("No such df in cache")
+
+        dict_name = name + '_df'
+        self.last_used[dict_name] = datetime.datetime.now()
+        if dict_name in self.memory:
+            return self.memory[dict_name]
+        else:
+            tmp = cache_utils.load_df(cache_utils.get_path_df(name))
+            self.memory[dict_name] = tmp
+            return tmp
 
     @staticmethod
     def cached_dfs():
@@ -66,23 +71,8 @@ class Cache:
         """
         dict_name = name + '_obj'
         self.memory[dict_name] = obj
+        self.last_used[dict_name] = datetime.datetime.now()
         cache_utils.save_obj(obj, cache_utils.get_path_obj(name))
-
-    def load_obj(self, name):
-        """
-        Loads object from cache
-        :param name: name of object
-        :return: object with given name
-        """
-        dict_name = name + '_obj'
-        if dict_name in self.memory:
-            return self.memory[dict_name]
-        elif os.path.exists(cache_utils.get_path_obj(name)):
-            tmp = cache_utils.load_obj(cache_utils.get_path_obj(name))
-            self.memory[dict_name] = tmp
-            return tmp
-        else:
-            raise KeyError("No such object in cache")
 
     def is_cached_obj(self, name):
         """
@@ -92,6 +82,24 @@ class Cache:
         """
         dict_name = name + '_obj'
         return dict_name in self.memory or os.path.exists(cache_utils.get_path_obj(name))
+
+    def load_obj(self, name):
+        """
+        Loads object from cache
+        :param name: name of object
+        :return: object with given name
+        """
+        if not self.is_cached_obj(name):
+            raise KeyError("No such object in cache")
+
+        dict_name = name + '_obj'
+        self.last_used[dict_name] = datetime.datetime.now()
+        if dict_name in self.memory:
+            return self.memory[dict_name]
+        else:
+            tmp = cache_utils.load_obj(cache_utils.get_path_obj(name))
+            self.memory[dict_name] = tmp
+            return tmp
 
     @staticmethod
     def cached_objs():
