277bb23de175f943617dc770ac598eb380b15d8f
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-05 12:59:46 +0300

    refactored Model class, added dosc

--- model/model.py
+++ model/model.py
@@ -1,38 +1,58 @@
-from skopt.space import Integer, Real, Categorical
-
 class Model:
     search_spaces = {}
-    fast_fit_params = {}
-    default_params = {}
     system_params = {}
+    system_fit_params = {}
     Estimator = None
     short_name = 'model'
     
-    def __init__(self, **kwargs): # (self, **kwargs)     ?
-        self.is_fit = 0     # self.params = kwargs ?
-#         self.params = params ## TODO: make read-only
-#         if not params:
-#             self.params = type(self).default_params
-        self.params = dict()
-        self.system_params = type(self).system_params
-        for param in type(self).default_params:
-            if param in kwargs:
-                self.params[param] = kwargs[param]
-        for param in type(self).system_params:
-            if param in kwargs:
-                self.system_params[param] = kwargs[param]
+    def __init__(self, params=None, n_jobs=-1, verbosity=False):
+        """
+        :param params: model hyperparameters
+        :param n_jobs: number of threads to use
+        :param verbosity: True or False (print logs or not)
+        """
+        self.is_fit = 0
+        self.params = params
+        self.n_jobs = n_jobs
+        self.verbosity = verbosity
         self.estimator = type(self).Estimator(**self.params, **type(self).system_params)
         self.reset_name()
-#         self.coeff = 1 # hack to make Ensemble simplification possible
         
     def reset_name(self):
+        """
+        Changes name according to current parameters.
+        :return:
+        """
         self.__name__ = f"{type(self).short_name}_{hex(hash(frozenset(self.params.items())))[-2:]}"
         
     def fit(self, X, y, **kwargs):
+        """
+        Fit model. You can call this method only once
+        :param X: data matrix
+        :param y: target
+        :param kwargs: additional params to be used for model fitting
+        :return:
+        """
         if self.is_fit:
             raise UserWarning(f"This {type(self).__name__} is already fit.\nUse .warm_fit() to fit it from current state.")
-        self.estimator.fit(X, y, **kwargs)
+        fit_params = {}
+        for param in type(self).system_fit_params:
+            if param in kwargs:
+                fit_params[param] = kwargs[param]
+            else:
+                fit_params[param] = type(self).system_fit_params[param]
+        self._fit(X, y, **fit_params)
         self.is_fit = True
+
+    def _fit(self, X, y, **kwargs):
+        """
+        This method is one of which you need to override to define a custom model.
+        :param X: data matrix
+        :param y: target values
+        :param kwargs: additional params
+        :return:
+        """
+        self.estimator.fit(X, y, **kwargs)
         
     def __setattr__(self, name, value):
         if name == 'params':
@@ -48,16 +68,12 @@ class Model:
             super().__setattr__(name, value)
         
     def predict(self, X):
+        """
+        Standard prediction method.
+        :param X: data matrix
+        :return:
+        """
         return self.estimator.predict(X)
-    
-#     def predict_proba(self, X):
-#         return self.estimator.predict_proba(X)
-    
-#     def get_params(self, *args, **kwargs):
-#         return self.estimator.get_params(*args, **kwargs)
-    
-#     def set_params(self, *args, **kwargs):
-#         return self.estimator.set_params(*args, **kwargs)
     
     def __mul__(self, x):
         return WeightedModel(self, x)
@@ -79,15 +95,12 @@ class Model:
         
     def __repr__(self):
         return self.__name__
-    
-    def preprocess_before_split(self, X):
-        return X
-    
-    def preprocess_after_split(self, X):
-        return X
         
 
 class WeightedModel(Model): # MulNode
+    """
+    Multiplies predictions by a certain coefficient.
+    """
     def __init__(self, model, coeff):
         self.model = model
         self.coeff = coeff
@@ -99,11 +112,20 @@ class WeightedModel(Model): # MulNode
             self.__name__ += f"({self.model.__name__})"
         else:
             self.__name__ += f"{self.model.__name__}"
-            
+
     def predict(self, X):
+        """
+        Standard prediction method.
+        :param X: data matrix
+        :return:
+        """
         return self.coeff * self.model.predict(X)
     
+
 class Ensemble(Model): # AddNode
+    """
+    Sums up all predictions of all models.
+    """
     def __init__(self, models):
         self.models = []
         ensembles = [model for model in models if type(model) == type(self)]
@@ -113,23 +135,32 @@ class Ensemble(Model): # AddNode
             _models += ensemble.models
         _models += single_models
         
-        def get_model_coeff(model):
+        def __get_model_coeff(model):
+            """
+            :param model:
+            :return: if the model is weighted, then returns its coefficient, else 1
+            """
             if 'model' in dir(model):
                 return model.model, model.coeff
             return model, 1
         
-        self.models = list(set([get_model_coeff(model)[0] for model in _models]))
+        self.models = list(set([__get_model_coeff(model)[0] for model in _models]))
         _coeffs = [0 for i in range(len(self.models))]
         for i in range(len(self.models)):
             for model in _models:
-                if get_model_coeff(self.models[i])[0] == get_model_coeff(model)[0]:
-                    _coeffs[i] += get_model_coeff(model)[1]
+                if __get_model_coeff(self.models[i])[0] == __get_model_coeff(model)[0]:
+                    _coeffs[i] += __get_model_coeff(model)[1]
         
         self.models = [WeightedModel(model, coeff) for model, coeff in zip(self.models, _coeffs)]
                     
         self.__name__ = ' + '.join([model.__name__ for model in self.models])
         
     def predict(self, X):
+        """
+        Standard prediction method.
+        :param X: data matrix
+        :return:
+        """
         res = 0
         for model in self.models:
             res += model.predict(X)
