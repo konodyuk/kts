1c890006ff8863d75bc9f5a4c9b12479b712fd48
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-05 17:20:08 +0300

    removed unused submodule

--- pipeline/pipeline.py
+++
@@ -1,9 +0,0 @@
-from .model import *
-
-class Pipeline(Model):
-    def __init__(self, model, featureset, splitter):
-        self.model = model
-        self.featureset = featureset
-        
-    def fit(self, df):
-        
