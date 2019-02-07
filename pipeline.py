bb9f3d711790a8b00eabf76ee6352151cdbda8ec
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-07 15:35:00 +0300

    implemented Pipeline class

---
+++ pipeline.py
@@ -0,0 +1,14 @@
+from .model import Model
+
+class Pipeline(Model):
+    def __init__(self, model, featureslice):
+        self.model = model
+        self.featureslice = featureslice
+        self.__name__ = self.model.__name__ + ':' + "undefined_fs"
+
+    def fit(self, **kwargs):
+        tmp = self.featureslice()
+        self.model.fit(tmp.values, self.featureslice.target, **kwargs)
+
+    def predict(self, df, **kwargs):
+        return self.model.predict(self.featureslice(df).values, **kwargs)
