6d3faf34894ac33e167776be5016916e8b9e69c9
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-09 17:13:39 +0300

    added progressbar visualization

--- validation/validator.py
+++ validation/validator.py
@@ -5,18 +5,22 @@ from .experiment import experiment_list, Experiment
 from ..pipeline import Pipeline
 # from ..feature.storage import FeatureSlice
 from copy import deepcopy
+import tqdm
 
 class Validator:
-    def __init__(self, splitter, metric):
+    def __init__(self, splitter, metric, enable_widget=False):
         self.splitter = splitter
         self.metric = metric
+        self.bar = (tqdm.tqdm_notebook if enable_widget else tqdm.tqdm)
         
     def score(self, model, featureset):
         pipelines = []
         scores = []
         oofs = np.zeros_like(featureset.target, dtype=np.float)
         weights = np.zeros_like(featureset.target, dtype=np.float)
-        for spl in self.splitter.split:
+        pbar = self.bar(self.splitter.split, total=self.splitter.size)
+        pbar.set_description_str(f"Val of {model.__name__}")
+        for spl in pbar:
             idx_train = spl['train']
             idx_test = spl['test']
             c_model = deepcopy(model)
@@ -29,6 +33,7 @@ class Validator:
             score = self.metric(featureset.target.values[idx_test], pred)
             pipelines.append(pl)
             scores.append(score)
+            pbar.set_postfix_str(f"score: {np.mean(scores)}")
         model_name = f"ens_{pipelines[0].model.__name__}_x{len(pipelines)}"
         final_ensemble = pipelines[0]
         for pipeline in pipelines[1:]:
