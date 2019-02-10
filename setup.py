39ca799e065117fbf9cee3d6306df38caba643f3
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-10 15:16:56 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('VERSION', 'r') as f:
+    VERSION = int(f.read().strip())
+
+VERSION += 1
+
+with open('VERSION', 'w') as f:
+    f.write(str(VERSION))
+
+setuptools.setup(
+    name="kts",
+    version=f"0.0.{VERSION}",
+    author="Nikita Konodyuk",
+    author_email="konodyuk@gmail.com",
+    description="Competition-oriented framework for interactive feature engineering and building pipelines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/konodyuk/kts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "mprop",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "scikit-optimize",
+        "matplotlib",
+        "dill",
+        "feather-format",
+        "xgboost",
+        "lightgbm",
+        "catboost"
+    ]
+)
