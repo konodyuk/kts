a8bf639baba16375d227b2e6c082a8525e6d4557
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-10 22:39:49 +0300

    updated versioning

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('kts/VERSION', 'r') as f:
+with open('VERSION', 'r') as f:
     VERSION = int(f.read().strip())
 
 VERSION += 1
 
-with open('kts/VERSION', 'w') as f:
+with open('VERSION', 'w') as f:
     f.write(str(VERSION))
 
 setuptools.setup(
@@ -41,8 +41,5 @@ setuptools.setup(
         "xgboost",
         "lightgbm",
         "catboost"
-    ],
-    package_data={
-        'kts': ['VERSION'],
-    }
+    ]
 )
