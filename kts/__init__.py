349096186791cff64bcab0835d526c131c29a982
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-10 15:33:12 +0300

    fixed deps again

--- kts/__init__.py
+++ kts/__init__.py
@@ -1,4 +1,4 @@
-from CLI.file_system_building import build_file_system
+from .CLI.file_system_building import build_file_system
 build_file_system()
 from .feature.decorators import preview, register, deregister, dropper, selector
 from .feature.stl import *
