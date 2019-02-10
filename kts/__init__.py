e57c00a52c278425b5d8f37e6ca9250f0406a9f6
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-10 22:38:50 +0300

    updated checking, removed version

--- kts/__init__.py
+++ kts/__init__.py
@@ -2,9 +2,7 @@ import sys
 import os
 sys.path.insert(0, '.')
 
-print(sys.argv)
-
-if True or not sys.argv[0]:
+if not sys.argv[0].endwith(os.path.split(__file__)[-1]):
     from .cli import check_file_system
     check_file_system()
     from .feature.decorators import preview, register, deregister, dropper, selector
@@ -12,7 +10,3 @@ if True or not sys.argv[0]:
     from .feature import stl
     from .feature.storage import feature_list as features
     from .validation.experiment import experiment_list as experiments
-
-    VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
-    with open(VERSION_FILE) as f:
-        __version__ = f"0.0.{int(f.read().strip()) + 1}"
