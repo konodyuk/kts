3dd9bfbe6f4298991f45810bc17181188070a15b
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-11 10:55:29 +0300

    now dont process case of direct call

--- kts/__init__.py
+++ kts/__init__.py
@@ -1,12 +1,11 @@
 import sys
-import os
 sys.path.insert(0, '.')
 
-if not sys.argv[0].endswith(os.path.split(__file__)[-1]):
-    from .cli import check_file_system
-    check_file_system()
-    from .feature.decorators import preview, register, deregister, dropper, selector
-    from .feature.stl import *
-    from .feature import stl
-    from .feature.storage import feature_list as features
-    from .validation.experiment import experiment_list as experiments
+
+from .cli import check_file_system
+check_file_system()
+from .feature.decorators import preview, register, deregister, dropper, selector
+from .feature.stl import *
+from .feature import stl
+from .feature.storage import feature_list as features
+from .validation.experiment import experiment_list as experiments
