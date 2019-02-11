6bb408c58f1755b2764e8fe2b1e9b9bbdc3c94c0
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-11 11:13:43 +0300

    removed kts-cli functionality

--- kts/cli/__init__.py
+++ kts/cli/__init__.py
@@ -1,17 +1,1 @@
-import argparse
-import sys
-from .file_system import build_file_system, check_file_system
-
-
-def run():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="subcommand",
-                                       title='subcommands',
-                                       description='valid subcommands',
-                                       help='Use init to initialize kts project inside of this folder.')
-    subparsers.add_parser('init')
-    args = parser.parse_args(sys.argv[1:])
-    if args.subcommand == 'init':
-        build_file_system()
-    else:
-        parser.print_help()
+from .file_system import check_file_system
