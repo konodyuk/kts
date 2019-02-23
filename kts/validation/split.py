b5eccd38950196aa6a76a4e8030c4fc072c7217c
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-23 19:18:31 +0300

    Holdout is from now on based on ShuffleSplit; added LeaveOneOut

--- kts/validation/split.py
+++ kts/validation/split.py
@@ -58,16 +58,36 @@ class NM(BaseSplitter):
         return self.n_folds * self.n_splits
 
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit
 class Holdout(BaseSplitter):
-    def __init__(self, y, test_size=0.3):
+    def __init__(self, y, test_size=0.3, stratify=False):
         self.sz = len(y)
+        self.y = y
         self.test_size = test_size
+        if stratify:
+            self.Splitter = StratifiedShuffleSplit
+        else:
+            self.Splitter = ShuffleSplit
 
     def _split(self):
-        idx_train, idx_test = train_test_split(np.arange(self.sz), test_size=self.test_size, shuffle=True, random_state=config.seed)
-        yield {'train': idx_train, 'test': idx_test}
+        for idx_train, idx_test in self.Splitter(
+            n_splits=1,
+            test_size=self.test_size,
+            random_state=self.seed
+        ).split(np.zeros(len(self.y)), self.y):
+            yield {'train': idx_train, 'test': idx_test}
 
     @property
     def size(self):
         return 1
+
+
+from sklearn.model_selection import LeaveOneOut as LOO
+class LeaveOneOut(BaseSplitter):
+    def __init__(self):
+        pass
+
+    def _split(self):
+        for idx_train, idx_test in LOO().split(np.zeros(len(self.y)), self.y):
+            yield {'train': idx_train, 'test': idx_test}
