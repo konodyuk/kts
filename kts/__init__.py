14d90d1decab6007189ed26a13fddab7a5f64e1a
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-24 15:57:58 +0300

    added alias for FeatureSet

--- kts/__init__.py
+++ kts/__init__.py
@@ -8,4 +8,5 @@ from .feature.decorators import preview, register, deregister, dropper, selector
 from .feature.stl import *
 from .feature import stl
 from .feature.storage import feature_list as features
+from .feature.storage import FeatureSet
 from .validation.experiment import experiment_list as experiments
