fe6ccf7561d46f4946e372e1e292487287f64bd5
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-24 16:23:12 +0300

    fixed trouble on import

--- kts/zoo/__init__.py
+++ kts/zoo/__init__.py
@@ -4,10 +4,11 @@ from . import regression
 from ..model import Model
 import forge
 
-model_classes = [model_class for model_class in module.__dict__.items()
-                 if isinstance(model_class, Model) and model_class.__name__ is not 'Model'
-                 for module in (classification,
-                                binary_classification,
-                                regression)]
+model_classes = []
+for module in (classification, binary_classification, regression):
+    model_classes += [model_class for model_class in module.__dict__.items()
+                      if isinstance(model_class, Model)
+                      and model_class.__name__ is not 'Model']
+
 for model_class in model_classes:
     model_class.__init__ = forge.copy(model_class.Estimator.__init__)(Model.__init__)
