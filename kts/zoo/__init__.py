4917b3d8290bb41f07f02da0547aac31c066202c
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-24 16:10:56 +0300

    added signature autocorrection

--- kts/zoo/__init__.py
+++ kts/zoo/__init__.py
@@ -1,3 +1,16 @@
 from . import classification
 from . import binary_classification
 from . import regression
+from ..model import Model
+import forge
+
+model_classes = []
+for model_class in module.__dict__.items():
+    if isinstance(model_class, Model) and model_class.__name__ is not 'Model':
+        for module in (classification,
+                       binary_classification,
+                       regression):
+            model_classes.append(model_class)
+
+for model_class in model_classes:
+    model_class.__init__ = forge.copy(model_class.Estimator.__init__)(Model.__init__)
