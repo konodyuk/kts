31162187da6baab7c0c2cc6dabe10b9db1100985
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-02-25 20:16:57 +0300

    added some aliases

--- kts/__init__.py
+++ kts/__init__.py
@@ -10,4 +10,6 @@ from .feature import stl
 from .feature.storage import feature_list as features
 from .feature.storage import FeatureSet
 from .validation.experiment import experiment_list as experiments
+from .validation import Validator
 from .modelling import model
+from .storage.dataframe import link
