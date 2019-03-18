63e15b96714098bce4dbea06daea3070212991cf
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-03-18 18:02:49 +0300

    inherit kts-estimators directly from their real implementations, add mixins for different tasks, remove dirty signature trickery

--- kts/zoo/binary_classification.py
+++ kts/zoo/binary_classification.py
@@ -1,68 +1,112 @@
 from ..modelling import *
-from .. import config
+
+
+class BinaryClassifierMixin(Model):
+    def predict(self, X, **kwargs):
+        return self.predict_proba(X, **kwargs)[:, 1]
+
 
 from xgboost import XGBClassifier as _XGBC
-class XGBClassifier(Model):
-    Estimator = _XGBC
-    tracked_params = ['n_estimators', 'max_depth']
-    fit_params = {'verbose': False}
+class XGBClassifier(_XGBC, BinaryClassifierMixin):
     short_name = 'xgb'
-    
+    tracked_params = [
+        'base_score',
+        'booster',
+        'colsample_bylevel',
+        'colsample_bytree',
+        'gamma',
+        'learning_rate',
+        'max_delta_step',
+        'max_depth',
+        'min_child_weight',
+        'missing',
+        'n_estimators',
+        'objective',
+        'reg_alpha',
+        'reg_lambda',
+        'scale_pos_weight',
+        'subsample'
+    ]
+
+
 from lightgbm import LGBMClassifier as _LGBMC
-class LGBMClassifier(Model):
-    Estimator = _LGBMC
-    search_spaces = {
-        'num_leaves': (2, 50),
-        'learning_rate': (0.01, 1.0),
-        'max_depth': (0, 50),
-        'min_data_in_leaf': (20, 200),
-        'max_bin': (2, 255),
-    }
-
-    system_params = {
-        "metric": 'binary_logloss',
-        "verbosity": 1,
-        "seed": config.seed}
+class LGBMClassifier(_LGBMC, BinaryClassifierMixin):
     short_name = 'lgb'
-    
-    def predict(self, X):
-        return self.estimator.predict_proba(X)[:, 1]
-    
+    tracked_params = [
+        'class_weight',
+        'boost',
+        'min_data_in_leaf',
+        'bagging_freq',
+        'subsample_freq',
+        'bagging_fraction',
+        'learning_rate',
+        'reg_lambda',
+        'feature_fraction',
+        'min_sum_hessian_in_leaf',
+        'boosting_type',
+        'subsample_for_bin',
+        'reg_alpha',
+        'tree_learner',
+        'colsample_bytree',
+        'min_child_weight',
+        'min_child_samples',
+        'subsample',
+        'max_depth',
+        'boost_from_average',
+        'num_leaves',
+        'objective',
+        'n_estimators',
+        'min_split_gain',
+    ]
+
+
 from catboost import CatBoostClassifier as _CBC
-class CBClassifier(Model):
-    Estimator = _CBC
+class CatBoostClassifier(_CBC, BinaryClassifierMixin):
     short_name = 'cb'
-    def __init__(self, params=None, n_jobs=-1, verbosity=0):
-        super().__init__(params, n_jobs, verbosity)
-        self.verbose = verbosity
-        self.estimator.thread_count = n_jobs
-        # self.estimator.verbose = verbosity
+    tracked_params = [
+        'iterations',
+        'learning_rate',
+        'l2_leaf_reg',
+        'bootstrap_type',
+        'bagging_temperature',
+        'depth',
+        'one_hot_max_size',
+        'leaf_estimation_method',
+        'nan_mode',
+        'feature_border_type',
+        'border_count',
+        'max_depth',
+        'colsample_bylevel',
+        'eval_metric'
+    ]
 
-    def _fit(self, X, y, **kwargs):
-        self.estimator.fit(X, y, verbose=self.verbose)
 
-    def predict(self, X):
-        return self.estimator.predict_proba(X)[:, 1]
+from sklearn.ensemble import RandomForestClassifier as _RFC
+class RandomForestClassifier(_RFC, BinaryClassifierMixin):
+    short_name = 'rf'
+    tracked_params = [
+        'bootstrap',
+        'class_weight',
+        'criterion',
+        'max_depth',
+        'max_features',
+        'max_leaf_nodes',
+        'min_impurity_decrease',
+        'min_impurity_split',
+        'min_samples_leaf',
+        'min_samples_split',
+        'min_weight_fraction_leaf',
+        'n_estimators',
+        'oob_score',
+    ]
 
-from sklearn.linear_model import LogisticRegression as _LOGR
-class LogisticRegression(Model):
-    Estimator = _LOGR
-    short_name = 'logr'
-    search_spaces = {
-        'C': [10**x for x in range(-4, 5)],
-        'penalty': ['l1', 'l2'],
-        'fit_intercept': [True, False],
-        'class_weight': [None, 'balanced']
-    }
-    # max_iter and warm_start are useless for liblinear solver
-    system_params = {
-        'tol': 0.0001,
-        'solver': 'liblinear',
-        'multi_class': 'auto',
-        'n_jobs': -1,
-        'verbose': 0,
-        'random_state': 42
-    }
 
-    def predict(self, X):
-        return self.estimator.predict_proba(X)[:, 1]
+from sklearn.linear_model import LogisticRegression as _LR
+class LogisticRegression(_LR, BinaryClassifierMixin):
+    short_name = 'lr'
+    tracked_params = [
+        'C',
+        'penalty',
+        'fit_intercept',
+        'class_weight',
+    ]
--- kts/zoo/classification.py
+++ kts/zoo/classification.py
@@ -1,62 +1,112 @@
 from ..modelling import *
-from .. import config
+
+
+class MultiClassifierMixin(Model):
+    def predict(self, X, **kwargs):
+        return self.predict_proba(X, **kwargs)
+
 
 from xgboost import XGBClassifier as _XGBC
-class XGBClassifier(Model):
-    Estimator = _XGBC
-    default_params = {
-        "max_depth": 3, 
-        "learning_rate": 0.1, 
-        "n_estimators": 100, 
-        "booster": 'gbtree', 
-        "gamma": 0, 
-        "min_child_weight": 1, 
-        "max_delta_step": 0, 
-        "subsample": 1, 
-        "colsample_bytree": 1, 
-        "colsample_bylevel": 1, 
-        "reg_alpha": 0, 
-        "reg_lambda": 1, 
-        "scale_pos_weight": 1, 
-        "base_score": 0.5,
-    }
-    search_spaces = {
-        'max_depth': (0, 50),
-        'learning_rate': (0.01, 1.0, 'log-uniform'),
-        'n_estimators': (50, 100),
-        'booster': ['gbtree', 'dart'],
-        'gamma': (1e-9, 0.5, 'log-uniform'),
-        'min_child_weight': (0, 10),
-        'max_delta_step': (0, 20),
-        'subsample': (0.01, 1.0, 'uniform'),
-        'colsample_bytree': (0.01, 1.0, 'uniform'),
-        'colsample_bylevel': (0.01, 1.0, 'uniform'),
-        'reg_alpha': (1e-9, 1.0, 'log-uniform'),
-        'reg_lambda': (1e-9, 1000, 'log-uniform'),
-        'scale_pos_weight': (1e-6, 500, 'log-uniform'),
-        'base_score': (0.2, 0.7, 'uniform')
-    }
-    system_params = {
-        "objective": 'binary:logistic', 
-        "silent": True,
-        "n_jobs": 1, 
-        "random_state": 0, 
-        "seed": config.seed
-    }
+class XGBClassifier(_XGBC, MultiClassifierMixin):
     short_name = 'xgb'
-    
-    
+    tracked_params = [
+        'base_score',
+        'booster',
+        'colsample_bylevel',
+        'colsample_bytree',
+        'gamma',
+        'learning_rate',
+        'max_delta_step',
+        'max_depth',
+        'min_child_weight',
+        'missing',
+        'n_estimators',
+        'objective',
+        'reg_alpha',
+        'reg_lambda',
+        'scale_pos_weight',
+        'subsample'
+    ]
+
+
 from lightgbm import LGBMClassifier as _LGBMC
-class LGBMClassifier(Model):
-    Estimator = _LGBMC
+class LGBMClassifier(_LGBMC, MultiClassifierMixin):
     short_name = 'lgb'
-    
-    
+    tracked_params = [
+        'class_weight',
+        'boost',
+        'min_data_in_leaf',
+        'bagging_freq',
+        'subsample_freq',
+        'bagging_fraction',
+        'learning_rate',
+        'reg_lambda',
+        'feature_fraction',
+        'min_sum_hessian_in_leaf',
+        'boosting_type',
+        'subsample_for_bin',
+        'reg_alpha',
+        'tree_learner',
+        'colsample_bytree',
+        'min_child_weight',
+        'min_child_samples',
+        'subsample',
+        'max_depth',
+        'boost_from_average',
+        'num_leaves',
+        'objective',
+        'n_estimators',
+        'min_split_gain',
+    ]
+
+
 from catboost import CatBoostClassifier as _CBC
-class CBClassifier(Model):
-    Estimator = _CBC
+class CatBoostClassifier(_CBC, MultiClassifierMixin):
     short_name = 'cb'
-    
-    def predict(self, X):
-        return self.estimator.predict_proba(X)
-    
+    tracked_params = [
+        'iterations',
+        'learning_rate',
+        'l2_leaf_reg',
+        'bootstrap_type',
+        'bagging_temperature',
+        'depth',
+        'one_hot_max_size',
+        'leaf_estimation_method',
+        'nan_mode',
+        'feature_border_type',
+        'border_count',
+        'max_depth',
+        'colsample_bylevel',
+        'eval_metric'
+    ]
+
+
+from sklearn.ensemble import RandomForestClassifier as _RFC
+class RandomForestClassifier(_RFC, MultiClassifierMixin):
+    short_name = 'rf'
+    tracked_params = [
+        'bootstrap',
+        'class_weight',
+        'criterion',
+        'max_depth',
+        'max_features',
+        'max_leaf_nodes',
+        'min_impurity_decrease',
+        'min_impurity_split',
+        'min_samples_leaf',
+        'min_samples_split',
+        'min_weight_fraction_leaf',
+        'n_estimators',
+        'oob_score',
+    ]
+
+
+from sklearn.linear_model import LogisticRegression as _LR
+class LogisticRegression(_LR, MultiClassifierMixin):
+    short_name = 'lr'
+    tracked_params = [
+        'C',
+        'penalty',
+        'fit_intercept',
+        'class_weight',
+    ]
