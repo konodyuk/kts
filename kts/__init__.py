325667cffb5568cae3cdb657f8e125d53cbd6edf
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-03-18 18:09:41 +0300

    improve aliases

--- kts/__init__.py
+++ kts/__init__.py
@@ -5,7 +5,6 @@ sys.path.insert(0, '.')
 from .cli import check_file_system
 check_file_system()
 from .feature.decorators import preview, register, deregister, dropper, selector
-from .feature.stl import *
 from .feature import stl
 from .feature.storage import feature_list as features
 from .feature.storage import FeatureSet
@@ -13,3 +12,4 @@ from .validation.experiment import experiment_list as experiments
 from .validation import Validator
 from .modelling import model
 from .storage.dataframe import link
+from .storage import cache
