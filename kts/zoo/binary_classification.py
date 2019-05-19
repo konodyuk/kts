e5e608353232702b8913b49b15e6919400f3d296
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-05-19 13:04:08 +0300

    remove CatBoost, XGBoost and LGBM from requirements, now they are available at kts.zoo only if preinstalled

--- kts/zoo/binary_classification.py
+++ kts/zoo/binary_classification.py
@@ -5,80 +5,91 @@ class BinaryClassifierMixin(Model):
     def predict(self, X, **kwargs):
         return self.predict_proba(X, **kwargs)[:, 1]
 
+try:
+    from xgboost import XGBClassifier as _XGBC
 
-from xgboost import XGBClassifier as _XGBC
-class XGBClassifier(BinaryClassifierMixin, _XGBC):
-    short_name = 'xgb'
-    tracked_params = [
-        'base_score',
-        'booster',
-        'colsample_bylevel',
-        'colsample_bytree',
-        'gamma',
-        'learning_rate',
-        'max_delta_step',
-        'max_depth',
-        'min_child_weight',
-        'missing',
-        'n_estimators',
-        'objective',
-        'reg_alpha',
-        'reg_lambda',
-        'scale_pos_weight',
-        'subsample'
-    ]
-
-
-from lightgbm import LGBMClassifier as _LGBMC
-class LGBMClassifier(BinaryClassifierMixin, _LGBMC):
-    short_name = 'lgb'
-    tracked_params = [
-        'class_weight',
-        'boost',
-        'min_data_in_leaf',
-        'bagging_freq',
-        'subsample_freq',
-        'bagging_fraction',
-        'learning_rate',
-        'reg_lambda',
-        'feature_fraction',
-        'min_sum_hessian_in_leaf',
-        'boosting_type',
-        'subsample_for_bin',
-        'reg_alpha',
-        'tree_learner',
-        'colsample_bytree',
-        'min_child_weight',
-        'min_child_samples',
-        'subsample',
-        'max_depth',
-        'boost_from_average',
-        'num_leaves',
-        'objective',
-        'n_estimators',
-        'min_split_gain',
-    ]
+    class XGBClassifier(BinaryClassifierMixin, _XGBC):
+        short_name = 'xgb'
+        tracked_params = [
+            'base_score',
+            'booster',
+            'colsample_bylevel',
+            'colsample_bytree',
+            'gamma',
+            'learning_rate',
+            'max_delta_step',
+            'max_depth',
+            'min_child_weight',
+            'missing',
+            'n_estimators',
+            'objective',
+            'reg_alpha',
+            'reg_lambda',
+            'scale_pos_weight',
+            'subsample'
+        ]
+except ImportError:
+    pass
 
 
-from catboost import CatBoostClassifier as _CBC
-class CatBoostClassifier(BinaryClassifierMixin, _CBC):
-    short_name = 'cb'
-    tracked_params = [
-        'iterations',
-        'learning_rate',
-        'l2_leaf_reg',
-        'bootstrap_type',
-        'bagging_temperature',
-        'depth',
-        'one_hot_max_size',
-        'leaf_estimation_method',
-        'nan_mode',
-        'feature_border_type',
-        'border_count',
-        'max_depth',
-        'colsample_bylevel',
-        'eval_metric'
-    ]
+try:
+    from lightgbm import LGBMClassifier as _LGBMC
+
+    class LGBMClassifier(BinaryClassifierMixin, _LGBMC):
+        short_name = 'lgb'
+        tracked_params = [
+            'class_weight',
+            'boost',
+            'min_data_in_leaf',
+            'bagging_freq',
+            'subsample_freq',
+            'bagging_fraction',
+            'learning_rate',
+            'reg_lambda',
+            'feature_fraction',
+            'min_sum_hessian_in_leaf',
+            'boosting_type',
+            'subsample_for_bin',
+            'reg_alpha',
+            'tree_learner',
+            'colsample_bytree',
+            'min_child_weight',
+            'min_child_samples',
+            'subsample',
+            'max_depth',
+            'boost_from_average',
+            'num_leaves',
+            'objective',
+            'n_estimators',
+            'min_split_gain',
+        ]
+except ImportError:
+    pass
+
+
+try:
+    from catboost import CatBoostClassifier as _CBC
+
+    class CatBoostClassifier(BinaryClassifierMixin, _CBC):
+        short_name = 'cb'
+        tracked_params = [
+            'iterations',
+            'learning_rate',
+            'l2_leaf_reg',
+            'bootstrap_type',
+            'bagging_temperature',
+            'depth',
+            'one_hot_max_size',
+            'leaf_estimation_method',
+            'nan_mode',
+            'feature_border_type',
+            'border_count',
+            'max_depth',
+            'colsample_bylevel',
+            'eval_metric'
+        ]
+except ImportError:
+    pass
 
 
 from sklearn.ensemble import RandomForestClassifier as _RFC
--- kts/zoo/regression.py
+++ kts/zoo/regression.py
@@ -5,79 +5,90 @@ class RegressorMixin(Model):
     pass
 
 
-from xgboost import XGBRegressor as _XGBR
-class XGBRegressor(RegressorMixin, _XGBR):
-    short_name = 'xgb'
-    tracked_params = [
-        'base_score',
-        'booster',
-        'colsample_bylevel',
-        'colsample_bytree',
-        'gamma',
-        'learning_rate',
-        'max_delta_step',
-        'max_depth',
-        'min_child_weight',
-        'missing',
-        'n_estimators',
-        'objective',
-        'reg_alpha',
-        'reg_lambda',
-        'scale_pos_weight',
-        'subsample'
-    ]
+try:
+    from xgboost import XGBRegressor as _XGBR
+
+    class XGBRegressor(RegressorMixin, _XGBR):
+        short_name = 'xgb'
+        tracked_params = [
+            'base_score',
+            'booster',
+            'colsample_bylevel',
+            'colsample_bytree',
+            'gamma',
+            'learning_rate',
+            'max_delta_step',
+            'max_depth',
+            'min_child_weight',
+            'missing',
+            'n_estimators',
+            'objective',
+            'reg_alpha',
+            'reg_lambda',
+            'scale_pos_weight',
+            'subsample'
+        ]
+except ImportError:
+    pass
 
 
-from lightgbm import LGBMRegressor as _LGBMR
-class LGBMRegressor(RegressorMixin, _LGBMR):
-    short_name = 'lgb'
-    tracked_params = [
-        'class_weight',
-        'boost',
-        'min_data_in_leaf',
-        'bagging_freq',
-        'subsample_freq',
-        'bagging_fraction',
-        'learning_rate',
-        'reg_lambda',
-        'feature_fraction',
-        'min_sum_hessian_in_leaf',
-        'boosting_type',
-        'subsample_for_bin',
-        'reg_alpha',
-        'tree_learner',
-        'colsample_bytree',
-        'min_child_weight',
-        'min_child_samples',
-        'subsample',
-        'max_depth',
-        'boost_from_average',
-        'num_leaves',
-        'objective',
-        'n_estimators',
-        'min_split_gain',
-    ]
+try:
+    from lightgbm import LGBMRegressor as _LGBMR
+
+    class LGBMRegressor(RegressorMixin, _LGBMR):
+        short_name = 'lgb'
+        tracked_params = [
+            'class_weight',
+            'boost',
+            'min_data_in_leaf',
+            'bagging_freq',
+            'subsample_freq',
+            'bagging_fraction',
+            'learning_rate',
+            'reg_lambda',
+            'feature_fraction',
+            'min_sum_hessian_in_leaf',
+            'boosting_type',
+            'subsample_for_bin',
+            'reg_alpha',
+            'tree_learner',
+            'colsample_bytree',
+            'min_child_weight',
+            'min_child_samples',
+            'subsample',
+            'max_depth',
+            'boost_from_average',
+            'num_leaves',
+            'objective',
+            'n_estimators',
+            'min_split_gain',
+        ]
+except ImportError:
+    pass
 
 
-from catboost import CatBoostRegressor as _CBR
-class CatBoostRegressor(RegressorMixin, _CBR):
-    short_name = 'cb'
-    tracked_params = [
-        'iterations',
-        'learning_rate',
-        'l2_leaf_reg',
-        'bootstrap_type',
-        'bagging_temperature',
-        'depth',
-        'one_hot_max_size',
-        'leaf_estimation_method',
-        'nan_mode',
-        'feature_border_type',
-        'border_count',
-        'max_depth',
-        'colsample_bylevel',
-        'eval_metric'
-    ]
+try:
+    from catboost import CatBoostRegressor as _CBR
+    class CatBoostRegressor(RegressorMixin, _CBR):
+        short_name = 'cb'
+        tracked_params = [
+            'iterations',
+            'learning_rate',
+            'l2_leaf_reg',
+            'bootstrap_type',
+            'bagging_temperature',
+            'depth',
+            'one_hot_max_size',
+            'leaf_estimation_method',
+            'nan_mode',
+            'feature_border_type',
+            'border_count',
+            'max_depth',
+            'colsample_bylevel',
+            'eval_metric'
+        ]
+except ImportError:
+    pass
 
 
 from sklearn.ensemble import RandomForestRegressor as _RFR
