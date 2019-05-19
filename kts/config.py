fdbfce89b377d4905b167e778b2e841c1ee67a6f
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-05-19 23:38:20 +0300

    add some parameters, add kts_config.py support

--- kts/config.py
+++ kts/config.py
@@ -1,13 +1,18 @@
 import numpy as np
 import mprop
+import sys
+
 
 seed = 31337
 np.random.seed(seed)
 seeds = np.random.randint(100, size=10)
 
 storage_path = '../storage/'
+root_dir = '../'
 index_prefix = "__kts__index_"
 preview_call = 0
+memory_limit = 4 * (1024 ** 3)  # 4 Gb
+mode = 'local'
 
 
 @property
@@ -31,3 +36,11 @@ def experiment_path(config):
 
 
 mprop.init()
+
+
+if mode == 'local':
+    sys.path.insert(0, root_dir)
+    try:
+        from kts_config import *
+    except:
+        pass
