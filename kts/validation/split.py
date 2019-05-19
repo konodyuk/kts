d133b71abf12ca9705793541cc68a800ef1ca261
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-05-19 20:25:26 +0300

    add Refiner for stacking

--- kts/validation/split.py
+++ kts/validation/split.py
@@ -1,99 +1,22 @@
-from .. import config
 import numpy as np
-
-class BaseSplitter:
-    def __init__(self, y, n_folds=5, seed=config.seed):
-        raise DeprecationWarning('Use splitters from sklearn')
-        self.y = y
-        self.n_folds = n_folds
-        self.seed = seed
-        
-    @property
-    def split(self):
-        yield from self._split()
-            
-    @property
-    def size(self):
-        return self.n_folds
-            
-        
-from sklearn.model_selection import StratifiedKFold
-class SKF(BaseSplitter):
-    def _split(self):
-        for idx_train, idx_test in StratifiedKFold(
-            self.n_folds, 
-            shuffle=True, 
-            random_state=self.seed
-        ).split(np.zeros(len(self.y)), self.y):
-            yield {'train': idx_train, 'test': idx_test}
-            
-        
-        
-from sklearn.model_selection import KFold
-class KF(BaseSplitter):
-    def _split(self):
-        for idx_train, idx_test in KFold(
-            self.n_folds, 
-            shuffle=True, 
-            random_state=self.seed
-        ).split(np.zeros(len(self.y)), self.y):
-            yield {'train': idx_train, 'test': idx_test}
-            
-
-class NM(BaseSplitter):
-    def __init__(self, y, Splitter, n_folds=5, n_splits=3):
-        raise DeprecationWarning('Use splitters from sklearn')
-        self.y = y
-        self.Splitter = Splitter
-        self.n_folds = n_folds
-        self.n_splits = n_splits
-      
-    def _split(self):
-        for i in range(self.n_splits):
-            yield from self.Splitter(self.y, 
-                                     self.n_folds,
-                                     config.seeds[i]).split
-            
-            
-    @property
-    def size(self):
-        return self.n_folds * self.n_splits
+from sklearn.model_selection import BaseCrossValidator
+from sklearn.utils import indexable
 
 
-from sklearn.model_selection import ShuffleSplit
-from sklearn.model_selection import StratifiedShuffleSplit
-class Holdout(BaseSplitter):
-    def __init__(self, y, test_size=0.3, stratify=False):
-        raise DeprecationWarning('Use splitters from sklearn')
-        self.sz = len(y)
-        self.y = y
-        self.test_size = test_size
-        if stratify:
-            self.Splitter = StratifiedShuffleSplit
-        else:
-            self.Splitter = ShuffleSplit
+class Refiner(BaseCrossValidator):
+    def __init__(self, outer_splitter, inner_splitter=None):
+        super().__init__()
+        self.outer_splitter = outer_splitter
+        self.inner_splitter = (inner_splitter if inner_splitter is not None else outer_splitter)
 
-    def _split(self):
-        for idx_train, idx_test in self.Splitter(
-            n_splits=1,
-            test_size=self.test_size,
-            random_state=self.seed
-        ).split(np.zeros(len(self.y)), self.y):
-            yield {'train': idx_train, 'test': idx_test}
+    def split(self, X, y=None, groups=None):
+        X, y, groups = indexable(X, y, groups)
+        print(groups)
+        for _, idx_test_outer in self.outer_splitter.split(X, y, groups):
+            for idx_train, idx_test in self.inner_splitter.split(X[idx_test_outer],
+                                                                 y[idx_test_outer] if y is not None else None,
+                                                                 groups[idx_test_outer] if groups is not None else None):
+                yield idx_test_outer[idx_train], idx_test_outer[idx_test]
 
-    @property
-    def size(self):
-        return 1
-
-
-# BUG: when using LOO split, we should score the final OOF-preds, but our current Validator will score
-# each "1-sample-fold" separately using given metric instead of aggregating the predictions and scoring them all at once
-# A possible solution is to set a flag "score_oofs=True" for LOO split and check it inside of Validator
-from sklearn.model_selection import LeaveOneOut
-class LOO(BaseSplitter):
-    def __init__(self):
-        pass
-
-    def _split(self):
-        for idx_train, idx_test in LeaveOneOut().split(np.zeros(len(self.y)), self.y):
-            yield {'train': idx_train, 'test': idx_test}
+    def get_n_splits(self, X=None, y=None, groups=None):
+        return self.outer_splitter.get_n_splits() * self.inner_splitter.get_n_splits()
