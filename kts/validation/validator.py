e20a718fd93f5cd61f552b166798a5cca4f0598b
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-06-12 16:15:16 +0300

    add more steps of validation

--- kts/validation/validator.py
+++ kts/validation/validator.py
@@ -43,19 +43,22 @@ class Validator:
         mb.write(f"Validation of {model_name}:")
         for idx_train, idx_test in mb:
             c_model = deepcopy(model)
-            fsl = featureset.slice(idx_train)
+            fsl = featureset.slice(idx_train, idx_test)
             pl = Pipeline(c_model, fsl)
-            pb = progress_bar(range(2), parent=mb)
-            mb.child.comment = f'training...'
+            pb = progress_bar(range(3), parent=mb)
+            mb.child.comment = f'computing the features...'
             pb.on_iter_begin()
             pb.update(0)
             fsl()
+            mb.child.comment = f'training...'
+            pb.on_iter_begin()
+            pb.update(1)
             try:
                 pl.fit(eval_set=[(fsl(idx_test).values, featureset.target.values[idx_test])], **fit_params)
             except:
                 mb.child.comment = 'failed to train with eval_set, training without it...'
                 pl.fit(**fit_params)
-            pb.update(1)
+            pb.update(2)
             mb.child.comment = 'validating...'
             pred = pl.predict(idx_test)
             pl.featureslice.compress()
@@ -68,7 +71,7 @@ class Validator:
                 score = self.metric(featureset.target.values[idx_test], pred)
             pipelines.append(pl)
             scores.append(score)
-            pb.update(2)
+            pb.update(3)
             mb.first_bar.comment = f"{round(np.mean(scores), 7)}"
         final_ensemble = Ensemble(pipelines)
         final_ensemble = final_ensemble / len(pipelines)
