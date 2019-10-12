3f4c7fb2aae04b65e2436f61aa158717545930c9
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-10-12 16:51:20 +0300

    add SourceMetaClass

--- kts/utils.py
+++ kts/utils.py
@@ -3,6 +3,7 @@ import inspect
 from itertools import zip_longest
 import numpy as np
 import time
+from abc import ABCMeta
 
 
 def captcha():
@@ -52,3 +53,43 @@ def is_helper(func):
            and isinstance(func.source, str) \
            and 'def' in func.source  # genius
 
+
+class BadSignatureException(Exception):
+    pass
+
+
+def _create_source(class_name, base_classes, methods):
+    base_class_names = ', '.join([bc.__name__ for bc in base_classes])
+    res = f"""class {class_name}({base_class_names}):\n"""
+    for name, meth in methods.items():
+        if name not in ['__module__', '__qualname__']:
+            try:
+                res += inspect.getsource(meth) + '\n'
+            except TypeError:
+                raise UserWarning(
+                    f'Unexpected static variable in class definition: {name}. Use instance variables instead.')
+    return res.strip()
+
+
+def _check_signatures(class_name, base_classes, methods):
+    for name, meth in methods.items():
+        for base_class in base_classes:
+            if name in base_class.__dict__ and name not in ['__module__', '__qualname__']:
+                base_meth = getattr(base_class, name)
+                sign = inspect.signature(meth)
+                base_sign = inspect.signature(base_meth)
+                if sign != base_sign:
+                    raise BadSignatureException(
+                        f"Signature discrepancy: {name}{sign} in {class_name}, "
+                        f"but {name}{base_sign} in {base_class.__name__}")
+
+
+class SourceMetaClass(ABCMeta):
+    def __new__(cls, class_name, base_classes, dict_of_methods):
+        cls.check_methods(dict_of_methods)
+        _check_signatures(class_name, base_classes, dict_of_methods)
+        dict_of_methods['class_source'] = _create_source(class_name, base_classes, dict_of_methods)
+        return type.__new__(cls, class_name, base_classes, dict_of_methods)
+
+    def check_methods(methods):
+        pass
