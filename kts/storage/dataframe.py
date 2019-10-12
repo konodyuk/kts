e5356e5d749144339879838e096c4eedf29d2bcf
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-10-12 16:50:00 +0300

    MAJOR: reimplement KTDF

--- kts/storage/dataframe.py
+++ kts/storage/dataframe.py
@@ -1,19 +1,8 @@
 import pandas as pd
 from copy import deepcopy
-import warnings
 
 
-class SubDF(pd.DataFrame):
-    def __getattr__(self, item):
-        return None
-
-    def __dict__(self):
-        return dict()
-
-# MAY WORK BUT NEEDS TESTING
-# SubDF = type('SubDF', (pd.DataFrame), {})
-
-class DataFrame(SubDF):
+class DataFrame(pd.DataFrame):
     """
     A wrapper over the standard DataFrame class.
     Complements it with .train and .encoders attributes.
@@ -51,57 +40,33 @@ class DataFrame(SubDF):
         return res
     ```
     """
-    def __init__(self, df, train=None, encoders=None, slice_id=None):
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', UserWarning)
 
-            if isinstance(df, DataFrame):
-                super().__setattr__('df', df.df)
-                super().__setattr__('slice_id', df.slice_id if isinstance(slice_id, type(None)) else slice_id)
-                super().__setattr__('train', df.train if isinstance(train, type(None)) else train)  # ALERT: may cause errors during constructing like DF(DF(df), train=True)
-                super().__setattr__('encoders', df.encoders if isinstance(encoders, type(None)) else encoders)  # not deepcopy to allow DF(df) init in FeatureConstructors
-            else:
-                super().__setattr__('df', df)
-                super().__setattr__('slice_id', "0" * 16 if isinstance(slice_id, type(None)) else slice_id)
-                super().__setattr__('train', False if isinstance(train, type(None)) else train)
-                super().__setattr__('encoders', dict() if isinstance(encoders, type(None)) else encoders)
+    _metadata = ['train', 'encoders', 'slice_id']
+    _accessors = frozenset(_metadata)
 
-    def __dir__(self):
-        return dir(self.df) + ['df', 'train', 'encoders', 'slice_id']
-
-    def __copy__(self):
-        return DataFrame(self.df, self.slice_id, self.train, deepcopy(self.encoders))
-
-    def __getattr__(self, key):
-        if key in ['train', 'encoders', 'df', 'slice_id']:
-            return super().__getattr__(key)
+    def __init__(self, df, train=None, encoders=None, slice_id=None, **kwargs):
+        if isinstance(df, DataFrame):
+            pd.DataFrame.__init__(self, df, **kwargs)
+            self.slice_id = df.slice_id if isinstance(slice_id, type(None)) else slice_id
+            self.train = df.train if isinstance(train, type(None)) else train
+            self.encoders = df.encoders if isinstance(encoders, type(None)) else encoders
         else:
-            tmp = self.df.__getattr__(key)
-            if isinstance(tmp, pd.DataFrame):
-                return DataFrame(tmp, self.train, self.encoders)
-            else:
-                return tmp
+            pd.DataFrame.__init__(self, df, **kwargs)
+            self.slice_id = "0" * 16 if isinstance(slice_id, type(None)) else slice_id
+            self.train = False if isinstance(train, type(None)) else train
+            self.encoders = dict() if isinstance(encoders, type(None)) else encoders
 
-    def __setattr__(self, key, value):
-        if key in ['train', 'encoders', 'df', 'slice_id']:
-            super().__setattr__(key, value)
+    def __copy__(self, deep=False):
+        if not deep:
+            return DataFrame(df=self, slice_id=self.slice_id, train=self.train, encoders=deepcopy(self.encoders))
         else:
-            try:
-                self.df.__setattr__(key, value)
-            except:
-                pass
+            return DataFrame(df=self.copy(deep=True), slice_id=self.slice_id, train=self.train, encoders=deepcopy(self.encoders))
 
-    def __setitem__(self, key, value):
-        self.df.__setitem__(key, value)
-
-    def __getitem__(self, key):
-        tmp = self.df.__getitem__(key)
-        if isinstance(tmp, pd.DataFrame):
-            return DataFrame(tmp, self.train, self.encoders)
-        else:
-            return tmp
+    @property
+    def _constructor(self):
+        return DataFrame
 
 
 def link(df, ktdf):
     assert isinstance(ktdf, DataFrame), 'Second dataframe should be of type KTDF, not pd.DF'
-    return DataFrame(df, train=ktdf.train, encoders=ktdf.encoders, slice_id=ktdf.slice_id)
+    return DataFrame(df=df, train=ktdf.train, encoders=ktdf.encoders, slice_id=ktdf.slice_id)
