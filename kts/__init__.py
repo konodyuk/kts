27b44b5da92c37534e8fec5137a8b0433a4a350e
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-10-14 21:55:07 +0300

    refactor imports

--- kts/__init__.py
+++ kts/__init__.py
@@ -1,25 +1,14 @@
-import sys
-
-sys.path.insert(0, ".")
-
-# from .environment import get_mode
-# get_mode()
-from . import config
 from kts.api.decorators import preview, register, deregister, dropper, selector, helper
-from . import stl
+from kts.api.feature import FeatureSet
 from kts.api.feature import feature_list as features
 from kts.api.helper import helper_list as helpers
-from kts.api.feature import FeatureSet
-from kts.validation.validator import Validator
-from kts.core.dataframe import link
-from kts.core.dataframe import DataFrame as KTDF
-from kts.core.backend.memory import save, load, ls, remove, rm
-from kts.validation.leaderboard import leaderboard
-from . import zoo
-from kts.modelling.stacking import stack
-from kts.modelling.custom_model import CustomModel
 from kts.core.backend.memory import cache
-from . import eda
-from kts import feature_selection
+from kts.core.backend.memory import save, load, ls, remove, rm
+from kts.core.dataframe import DataFrame as KTDF
+from kts.core.dataframe import link
+from kts.modelling.custom_model import CustomModel
+from kts.modelling.stacking import stack
+from kts.validation.leaderboard import leaderboard
+from kts.validation.validator import Validator
 
 lb = leaderboard
