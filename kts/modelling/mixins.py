58ad3df7f0ff8e76984e9294ad33efbbfa02e13a
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-01-29 23:13:08 +0300

    implement ProgressMixin, remove ArithmeticMixin, change naming algorithm

--- kts/modelling/mixins.py
+++ kts/modelling/mixins.py
@@ -1,55 +1,28 @@
-import numpy as np
-from hashlib import sha256
-import json
+from kts.util.hashing import hash_dict, hash_str
 
 
 class NamingMixin:
-    """ """
     @property
-    def __name__(self):
+    def name(self):
         try:
-            ctx_short_name = self.short_name
+            tracked_params = self.tracked_params
         except:
-            ctx_short_name = self.get_short_name()
-        try:
-            ctx_tracked_params = self.tracked_params
-        except:
-            ctx_tracked_params = self.get_tracked_params()
-        self.params = {
+            tracked_params = self.get_tracked_params()
+        params = {
             key: self.get_params()[key]
-            for key in ctx_tracked_params if key in self.get_params()
+            for key in tracked_params if key in self.get_params()
         }
-        return f"{ctx_short_name}_{sha256((json.dumps(self.params, sort_keys=True)).encode()).hexdigest()[-3:] if self.params else 'default'}"
 
-
-class ArithmeticMixin:
-    """ """
-    def __mul__(self, x):
-        return WeightedModel(self, x)
-
-    def __rmul__(self, x):
-        return self * x
-
-    def __truediv__(self, x):
-        assert x != 0
-        return WeightedModel(self, 1.0 / x)
-
-    def __add__(self, other):
-        # assert type(self).__base__ == type(other).__base__, \
-        # f"Can't add {type(other).__name__} to {type(self).__name__}."
-        if other == 0:
-            return self
-        return Ensemble([self, other])
-
-    def __radd__(self, other):
-        return self + other
+        name = self.__class__.__name__
+        name += hash_dict(params, 3)
+        if 'class_source' in dir(self):
+            name += hash_str(self.class_source, 2)
+        return name
 
 
 class SourceMixin:
-    """ """
     @property
     def source(self):
-        """ """
         args = []
         for key, value in self.get_params().items():
             args.append(f"{key}={repr(value)}")
@@ -58,173 +31,42 @@ class SourceMixin:
 
 
 class PreprocessingMixin:
-    """ """
-    def preprocess(self, X, y):
-        """Preprocess input before feeding it into model
-
-        Args:
-          X: np.array
-          y: np.array or None (fitting or inference)
-
-        Returns:
-          X_processed, y_processed)
-
-        """
+    def preprocess(self, X, y=None):
         return X, y
 
     def preprocess_fit(self, X, y, *args, **kwargs):
-        """
-
-        Args:
-          X:
-          y:
-          *args:
-          **kwargs:
-
-        Returns:
-
-        """
         X_proc, y_proc = self.preprocess(X, y)
         self.fit(X_proc, y_proc, *args, **kwargs)
 
     def preprocess_predict(self, X, *args, **kwargs):
-        """
-
-        Args:
-          X:
-          *args:
-          **kwargs:
-
-        Returns:
-
-        """
         X_proc, _ = self.preprocess(X, None)
         return self.predict(X_proc, *args, **kwargs)
 
 
-class Model(ArithmeticMixin, NamingMixin, SourceMixin, PreprocessingMixin):
-    """ """
-    def __repr__(self):
-        return f"[{self.__name__}] {self.source}"
+class ProgressMixin:
+    def progress_callback(self, line):
+        return {'success': False}
+
+    def enable_verbosity(self):
+        pass
+
+    def get_n_steps(self):
+        return 1
 
 
-class WeightedModel(ArithmeticMixin, PreprocessingMixin):  # MulNode
-    """Multiplies predictions by a certain coefficient."""
-    def __init__(self, model, coeff):
-        self.model = model
-        self.coeff = coeff
-        if type(self.model) == type(self):
-            self.coeff *= self.model.coeff
-            self.model = self.model.model
-        self.__name__ = f"{round(self.coeff, 2)} * "
-        if "+" in self.model.__name__:
-            self.__name__ += f"({self.model.__name__})"
-        else:
-            self.__name__ += f"{self.model.__name__}"
-
-    def predict(self, X):
-        """Standard prediction method.
-
-        Args:
-          X: data matrix
-
-        Returns:
-
-        """
-        return self.coeff * self.model.preprocess_predict(X)
-
-
-class Ensemble(ArithmeticMixin, PreprocessingMixin):  # AddNode
-    """Sums up all predictions of all models."""
-    def __init__(self, models):
-        self.models = []
-        ensembles = [model for model in models if type(model) == type(self)]
-        single_models = [model for model in models if type(model) != type(self)]
-        _models = []
-        for ensemble in ensembles:
-            _models += ensemble.models
-        _models += single_models
-
-        def __get_model_coeff(model):
-            """
-            :param model:
-            :return: if the model is weighted, then returns its coefficient, else 1
-            """
-            if isinstance(model, WeightedModel):
-                return model.model, model.coeff
-            return model, 1
-
-        self.models = list(
-            set([__get_model_coeff(model)[0] for model in _models]))
-        _coeffs = np.zeros(len(self.models))
-        for i in range(len(self.models)):
-            for model in _models:
-                if __get_model_coeff(
-                        self.models[i])[0] == __get_model_coeff(model)[0]:
-                    _coeffs[i] += __get_model_coeff(model)[1]
-
-        self.models = [
-            WeightedModel(model, coeff)
-            for model, coeff in zip(self.models, _coeffs)
-        ]
-        self.norm_coeff = _coeffs.sum()
-        _coeffs /= self.norm_coeff
-        self.__name__ = " + ".join([(model / self.norm_coeff).__name__
-                                    for model in self.models])
-
-    def predict(self, X):
-        """Standard prediction method.
-
-        Args:
-          X: data matrix
-
-        Returns:
-
-        """
-        res = 0
-        for model in self.models:
-            res += model.preprocess_predict(X)
-        return res / self.norm_coeff
-
-    def __mul__(self, x):
-        return Ensemble([model * x for model in self.models])
-
-    def __truediv__(self, x):
-        assert x != 0
-        return Ensemble([model / x for model in self.models])
+class Model(NamingMixin, SourceMixin, PreprocessingMixin, ProgressMixin):
+    pass
 
 
 class BinaryClassifierMixin(Model):
-    """ """
     def predict(self, X, **kwargs):
-        """
-
-        Args:
-          X:
-          **kwargs:
-
-        Returns:
-
-        """
         return self.predict_proba(X, **kwargs)[:, 1]
 
 
 class MultiClassifierMixin(Model):
-    """ """
     def predict(self, X, **kwargs):
-        """
-
-        Args:
-          X:
-          **kwargs:
-
-        Returns:
-
-        """
         return self.predict_proba(X, **kwargs)
 
 
 class RegressorMixin(Model):
-    """ """
-
     pass
