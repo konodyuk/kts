0483b5c9d6ddd8b76b04c5b7a6a502b645301ba4
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-01-29 23:24:00 +0300

    change imports

--- kts/api/__init__.py
+++ kts/api/__init__.py
@@ -1,14 +1,13 @@
-from kts.api.decorators import preview, register, deregister, dropper, selector, helper
+from kts.api.decorators import preview, feature, helper, generic
 from kts.core.feature_set import FeatureSet
-from kts.api.feature import feature_list as features
-from kts.api.helper import helper_list as helpers
-from kts.core.backend.memory import cache
-from kts.core.backend.memory import save, load, ls, remove, rm
-from kts.core.dataframe import DataFrame as KTDF
-from kts.core.dataframe import link
+from kts.core.lists import feature_list, helper_list
+# from kts.core.backend.memory import cache
+# from kts.core.backend.memory import save, load, ls, remove, rm
+from kts.core.frame import KTSFrame
 from kts.modelling.custom_model import CustomModel
-from kts.modelling.stacking import stack
-from kts.validation.leaderboard import leaderboard
+# from kts.modelling.stacking import stack
+from kts.validation.leaderboard import leaderboard, leaderboard_list
 from kts.validation.validator import Validator
 
 lb = leaderboard
+lbs = leaderboard_list
