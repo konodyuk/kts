59f64cb206c841e1d542a2f14346f76c6dac8012
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-01-29 23:07:19 +0300

    reimplement Experiment, implement ExperimentAlias

--- kts/validation/experiment.py
+++ kts/validation/experiment.py
@@ -1,147 +1,122 @@
-import pandas as pd
-import texttable as tt
-from fastprogress import progress_bar as pb
+import time
 
-from kts.feature_selection.selector import BuiltinImportance
-from kts.util.misc import hash_str
-from kts.eda.importance import plot_importances
-from kts.modelling.mixins import ArithmeticMixin
+import numpy as np
+
+from kts.core import ui
+from kts.util.hashing import hash_str
 
 
-class Experiment(ArithmeticMixin):
-    """ """
+def experiment_id(model, feature_set):
+    return hash_str(model.name + feature_set.name)
+
+
+class Experiment:
     def __init__(
-            self,
-            pipeline,
-            oof,
-            score,
-            std,
-            description,
-            validator,
-            feature_list,
-            helper_list,
+        self, 
+        cv_pipeline, 
+        oof, 
+        description,
+        validator
     ):
-        self.pipeline = pipeline
-        self.model = self.pipeline.models[0].model.model  # TODO: test out
-        self.model_name = self.model.__class__.__name__
-        self.parameters = self.model.get_params()
-        self.models = [model.model.model for model in self.pipeline.models]
-        self.featureset = self.pipeline.models[0].model.featureslice.featureset
-        self.tie_featuresets()
+        self.cv_pipeline = cv_pipeline
         self.oof = oof
-        self.score = score
-        self.std = std
-        self.identifier = hash_str(f"{pipeline.__name__}")[:6].upper()
-        self.oof.columns = [
-            col_name.replace("prediction", self.identifier)
-            for col_name in self.oof.columns
-        ]
-        self.__doc__ = description if description is not None else "no description"
-        self.__name__ = f"{self.identifier}-{round(score, 4)}-{pipeline.__name__}"
+        self.description = description
         self.validator = validator
-        self.features = list(feature_list)
-        self.helpers = list(helper_list)
+        self.id = experiment_id(self.model, self.feature_set)
+        self.date = time.time()
 
-    def __str__(self):
-        string = f"({round(self.score, 5)}, std:{round(self.std, 3)}: \n\tModel: {self.pipeline.__name__})"
-        return string
+    @property
+    def model(self):
+        return self.cv_pipeline.model
 
-    def predict(self, df):
-        """
+    @property
+    def feature_set(self):
+        return self.cv_pipeline.feature_set
+    
+    @property
+    def score(self):
+        return np.mean(self.cv_pipeline.scores)
+    
+    @property
+    def std(self):
+        return np.std(self.cv_pipeline.scores)
 
-        Args:
-          df: 
+    @property
+    def took(self):
+        return self.cv_pipeline.took
+    
+    def predict(self, frame):
+        return self.cv_pipeline.predict(frame)
 
-        Returns:
+    def feature_importances(self, plot=True):
+        raise NotImplemented
 
-        """
-        return self.pipeline.predict(df)
+    def move_to(self, lb_name: str):
+        raise NotImplemented
 
-    def __repr__(self):
-        fields = {
-            "Score":
-            f"{round(self.score, 7)}, std: {round(self.std, 7)} ({self.validator.metric.__name__})",
-            "Identifier": self.identifier,
-            "Description": self.__doc__,
-            # 'Model': self.model_name + f'\tx{len(self.models)}',
-            # 'Model parameters': self.parameters,
-            "Model": f"{self.model.__name__}\t x{len(self.models)}",
-            "|- source ": self.model.
-            source,  # be careful with refactoring: if you remove this space,
-            "FeatureSet": self.featureset.__name__,  #
-            "|- description": self.featureset.__doc__,  #
-            "|- source": self.featureset.__repr__(
-            ),  # both "source" rows will be considered identical
-            "Splitter": self.validator.splitter,
-        }
+    @property
+    def alias(self):
+        return ExperimentAlias(self)
 
-        table = tt.Texttable(max_width=80)
-        for field in fields:
-            table.add_row([field, fields[field]])
-        return table.draw()
+    @property
+    def _html_elements(self):
+        elements = [
+            ui.Annotation('ID'),
+            ui.Field(self.id),
+            ui.Annotation('score'),
+            ui.Field(self.score),
+            ui.Annotation('description'),
+            ui.Field(self.description),
+            ui.Annotation('model'),
+            ui.Field(self.model.__class__),
+            ui.Annotation('params'),
+            ui.Field(self.model.get_params()),
+            ui.Annotation('features'),
+            self.feature_set.feature_pool,
+            ui.Annotation('details'),
+            ui.Pool([
+                ui.Raw(self.feature_set.html_collapsible),
+                ui.Raw(self.model.html_collapsible),
+                ui.Raw(self.validator.html_collapsible)
+            ])
+        ]
+        return elements
+    
+    @property
+    def html(self):
+        return ui.Column([ui.Title('experiment')] + self._html_elements).html
+    
+    def html_collapsible(self, thumbnail=None, border=False):
+        cssid = np.random.randint(1000000000)
+        if thumbnail is None:
+            thumbnail = ui.ThumbnailField(f"{self.id}", cssid)
+        else:
+            thumbnail.cssid = cssid
+        elements = [ui.TitleWithCross('experiment', cssid)]
+        elements += self._html_elements
+        return ui.CollapsibleColumn(elements, thumbnail, cssid, outer=True, border=border).html
+    
 
-    def as_dict(self):
-        """ """
-        fields = {
-            "Score": self.score,
-            "std": self.std,
-            "ID": self.identifier,
-            "Model": self.model.__name__,
-            "FS": self.featureset.__name__,
-            "Description": self.__doc__,
-            "FS description": self.featureset.__doc__,
-            "Model source": self.model.source,
-            "FS source": self.featureset.__repr__(),
-            "Splitter": repr(self.validator.splitter),
-        }
-        for field in fields:
-            fields[field] = [fields[field]]
-        return fields
 
-    def as_df(self):
-        """ """
-        return pd.DataFrame(self.as_dict()).set_index("ID")
+class ExperimentAlias:
+    def __init__(self, experiment):
+        self._html_elements = experiment._html_elements
+        self.id = experiment.id
+        self.score = experiment.score
+        self.n_features = len(experiment.feature_set.before_split) + len(experiment.feature_set.before_split)
+        self.date = experiment.date
+        self.took = experiment.took
 
-    def feature_importances(self,
-                            plot=False,
-                            importance_calculator=BuiltinImportance(),
-                            **kw):
-        """
-
-        Args:
-          plot:  (Default value = False)
-          importance_calculator:  (Default value = BuiltinImportance())
-          **kw: 
-
-        Returns:
-
-        """
-        if plot:
-            return plot_importances(self,
-                                    calculator=importance_calculator,
-                                    **kw)
-        res = pd.DataFrame()
-        for w_model in pb(self.pipeline.models):
-            fsl = w_model.model.featureslice
-            model = w_model.model.model
-            importances = importance_calculator.calc(model, fsl, self)
-            importances = {k: [v] for k, v in importances.items()}
-            res = res.append(pd.DataFrame(importances), ignore_index=True)
-        return res
-
-    def set_df(self, df_input):
-        """
-
-        Args:
-          df_input: 
-
-        Returns:
-
-        """
-        self.tie_featuresets()
-        self.featureset.set_df(df_input)
-
-    def tie_featuresets(self):
-        """ """
-        for i in range(len(self.pipeline.models)):
-            self.pipeline.models[i].model.featureslice.featureset = self.featureset
+    @property
+    def html(self):
+        return ui.Column([ui.Title('experiment')] + self._html_elements).html
+    
+    def html_collapsible(self, thumbnail=None, border=False):
+        cssid = np.random.randint(1000000000)
+        if thumbnail is None:
+            thumbnail = ui.ThumbnailField(f"{self.id}", cssid)
+        else:
+            thumbnail.cssid = cssid
+        elements = [ui.TitleWithCross('experiment', cssid)]
+        elements += self._html_elements
+        return ui.CollapsibleColumn(elements, thumbnail, cssid, outer=True, border=border).html
