186742bfecf6f06f0ba12a95454b7aa770f57371
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-01-29 23:22:23 +0300

    implement progress tracking for CatBoost, LGBM, XGB

--- kts/zoo/binary_classification.py
+++ kts/zoo/binary_classification.py
@@ -1,11 +1,9 @@
-from kts.modelling.mixins import BinaryClassifierMixin
+from kts.modelling.mixins import BinaryClassifierMixin, ProgressMixin
 
 try:
     from xgboost import XGBClassifier as _XGBC
 
-    class XGBClassifier(BinaryClassifierMixin, _XGBC):
-        """ """
-
+    class XGBClassifier(BinaryClassifierMixin, _XGBC, ProgressMixin):
         short_name = "xgb"
         tracked_params = [
             "base_score",
@@ -26,15 +24,32 @@ try:
             "subsample",
         ]
 
+        def enable_verbosity(self):
+            self.set_params({'silent': False})
+
+        def progress_callback(self, line):
+            pos = s.find(']')
+            if pos != -1:
+                step = s[1:pos]
+                if not step.isdigit():
+                    return {'step': 0, 'train_score': 0, 'valid_score': 0, 'success': False}
+            else:
+                return{'step': 0, 'train_score': 0, 'valid_score': 0, 'success': False}
+            s = s.split(':')
+            valid_score = s[1]
+            return {'step': step, 'train_score': None, 'valid_score': valid_score, 'success': True}
+
+        def get_n_steps(self):
+            return self.get_params()['n_estimators']
+
+
 except ImportError:
     pass
 
 try:
     from lightgbm import LGBMClassifier as _LGBMC
 
-    class LGBMClassifier(BinaryClassifierMixin, _LGBMC):
-        """ """
-
+    class LGBMClassifier(BinaryClassifierMixin, _LGBMC, ProgressMixin):
         short_name = "lgb"
         tracked_params = [
             "class_weight",
@@ -63,15 +78,31 @@ try:
             "min_split_gain",
         ]
 
+        def enable_verbosity(self):
+            self.set_params({'silent': False})
+
+        def progress_callback(self, line):
+            pos = s.find(']')
+            if pos != -1:
+                step = s[1:pos]
+                if not step.isdigit():
+                    return {'step': 0, 'train_score': 0, 'valid_score': 0, 'success': False}
+            else:
+                return {'step': 0, 'train_score': 0, 'valid_score': 0, 'success': False}
+            s = s.split(':')
+            valid_score = s[1].strip()
+            return {'step': step, 'train_score': None, 'valid_score': valid_score, 'success': True}
+
+        def get_n_steps(self):
+            return self.get_params()['n_estimators']
+
 except ImportError:
     pass
 
 try:
     from catboost import CatBoostClassifier as _CBC
 
-    class CatBoostClassifier(BinaryClassifierMixin, _CBC):
-        """ """
-
+    class CatBoostClassifier(BinaryClassifierMixin, _CBC, ProgressMixin):
         short_name = "cb"
         tracked_params = [
             "iterations",
@@ -90,15 +121,37 @@ try:
             "eval_metric",
         ]
 
+        def enable_verbosity(self):
+            self.set_params({'verbose': True})
+
+        def progress_callback(self, line):
+            pos = s.find(':')
+            if pos != -1:
+                step = s[:pos]
+                if not step.isdigit():
+                    return {'step': 0, 'train_score': 0, 'valid_score': 0, 'success': False}
+            else:
+                return {'step': 0, 'train_score': 0, 'valid_score': 0, 'success': False}
+            s = s.split()
+            if 'learn' in s[1]:
+                train_score = s[2]
+                valid_score = s[4]
+            else:
+                valid_score = s[2]
+                train_score = None
+            return {'step': step, 'train_score': train_score, 'valid_score': valid_score, 'success': True}
+
+        def get_n_steps(self):
+            return self.get_params()['iterations']
+
 except ImportError:
     pass
+
 
 from sklearn.ensemble import RandomForestClassifier as _RFC
 
 
 class RandomForestClassifier(BinaryClassifierMixin, _RFC):
-    """ """
-
     short_name = "rf"
     tracked_params = [
         "bootstrap",
@@ -121,7 +174,5 @@ from sklearn.linear_model import LogisticRegression as _LR
 
 
 class LogisticRegression(BinaryClassifierMixin, _LR):
-    """ """
-
     short_name = "lr"
     tracked_params = ["C", "penalty", "fit_intercept", "class_weight"]
