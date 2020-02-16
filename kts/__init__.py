0dafdff133b9e07b1d489955ae5da0b0b077663c
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-02-16 22:20:37 +0300

    disable init inside ray

--- kts/__init__.py
+++ kts/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if not sys.argv[0].endswith('kts'):
+if not sys.argv[0].endswith('kts') and not sys.argv[0].endswith('default_worker.py'):
     from kts.api import *
     from kts.core.init import init
     init()
