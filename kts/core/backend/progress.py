3047a5f7a466dbc3f4be0f7aa96ccff8ddf6403b
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-02-20 10:46:10 +0300

    add conditional progressbar wrapper, fix eta calculations

--- kts/core/backend/progress.py
+++ kts/core/backend/progress.py
@@ -1,7 +1,11 @@
 import time
+from contextlib import contextmanager
+from copy import copy
 from typing import Iterable, Optional
 
 from ray.experimental import signal as rs
+
+from kts.core.backend.util import in_worker
 
 
 class AbstractProgressBar:
@@ -17,52 +21,10 @@ class AbstractProgressBar:
 class LocalProgressBar(AbstractProgressBar):
     _min_interval = 0.2
     report = None
+    run_id = None
 
-    def __init__(self, iterable: Iterable, total: Optional[int] = None):
-    # def __init__(self, iterable, total=None):
-        self.iterable = iterable
-        self.total = total
-        if total is None:
-            try:
-                self.total = len(self.iterable)
-            except:
-                pass
-        self.took = None
-        self.eta = None
-
-    def __iter__(self):
-        last_update = 0.
-        for i, o in enumerate(self.iterable):
-            yield o
-            cur = time.time()
-            self.update_time(i, cur)
-            if cur - last_update >= self._min_interval:
-                # rs.send(ProgressSignal(value=i + 1, total=self.total))
-                self.report.update(self.run_id, self.value, self.total, self.took, self.eta)
-                last_update = cur
-        # rs.send(ProgressSignal(value=self.total, total=self.total))
-        self.report.update(self.run_id, self.total, self.total, self.took, self.eta)
-
-
-class ProgressSignal(rs.Signal):
-    def __init__(self, value, total, took, eta):
-        self.value = value
-        self.total = total
-        self.took = took
-        self.eta = eta
-
-    def get_percentage(self):
-        return self.value / self.total * 100
-
-    def get_contents(self):
-        return {'value': self.value, 'total': self.total, 'took': self.took, 'eta': self.eta}
-
-
-class RemoteProgressBar(AbstractProgressBar):
-    _min_interval = 0.5
-
-    def __init__(self, iterable: Iterable, total: Optional[int] = None):
-    # def __init__(self, iterable, total=None):
+    def __init__(self, iterable: Iterable, total: Optional[int] = None, title: Optional[str] = None):
+        self.run_id = copy(self.__class__.run_id)
         self.iterable = iterable
         self.total = total
         if total is None:
@@ -73,17 +35,87 @@ class RemoteProgressBar(AbstractProgressBar):
         self.took = None
         self.eta = None
         self.start = None
+        if title is not None:
+            self.run_id.function_name += f" [{title}]"
 
     def __iter__(self):
         last_update = 0.
+        self.report.update(self.run_id, 0, self.total, None, None)
+        self.update_time(0, time.time())
         for i, o in enumerate(self.iterable):
             yield o
             cur = time.time()
-            self.update_time(i, cur)
+            self.update_time(i + 1, cur)
             if cur - last_update >= self._min_interval:
-                rs.send(ProgressSignal(value=i + 1, total=self.total, took=self.took, eta=self.eta))
+                self.report.update(self.run_id, i + 1, self.total, self.took, self.eta)
                 last_update = cur
-        rs.send(ProgressSignal(value=self.total, total=self.total, took=self.took, eta=None))
+        self.report.update(self.run_id, self.total, self.total, self.took, self.eta)
 
 
-pbar = RemoteProgressBar
+class ProgressSignal(rs.Signal):
+    def __init__(self, value, total, took, eta, title=None):
+        self.value = value
+        self.total = total
+        self.took = took
+        self.eta = eta
+        self.title = title
+
+    def get_percentage(self):
+        return self.value / self.total * 100
+
+    def get_contents(self):
+        return {'value': self.value, 'total': self.total, 'took': self.took, 'eta': self.eta, 'title': self.title}
+
+
+class RemoteProgressBar(AbstractProgressBar):
+    _min_interval = 0.2
+
+    def __init__(self, iterable: Iterable, total: Optional[int] = None, title: Optional[str] = None):
+        self.iterable = iterable
+        self.total = total
+        if total is None:
+            try:
+                self.total = len(self.iterable)
+            except:
+                pass
+        self.took = None
+        self.eta = None
+        self.start = None
+        self.title = title
+
+    def __iter__(self):
+        last_update = 0.
+        rs.send(ProgressSignal(value=0, total=self.total, took=None, eta=None, title=self.title))
+        self.update_time(0, time.time())
+        for i, o in enumerate(self.iterable):
+            yield o
+            cur = time.time()
+            self.update_time(i + 1, cur)
+            if cur - last_update >= self._min_interval:
+                rs.send(ProgressSignal(value=i + 1, total=self.total, took=self.took, eta=self.eta, title=self.title))
+                last_update = cur
+        rs.send(ProgressSignal(value=self.total, total=self.total, took=self.took, eta=None, title=self.title))
+
+
+class KTSProgressBar:
+    def __init__(self):
+        self.report = None
+        self.run_id = None
+
+    def __call__(self, iterable: Iterable, total: Optional[int] = None, title: Optional[str] = None):
+        if in_worker():
+            cls = RemoteProgressBar
+        else:
+            cls = LocalProgressBar
+            cls.report = self.report
+            cls.run_id = self.run_id
+        return cls(iterable, total, title)
+
+    @contextmanager
+    def local_mode(self, report, run_id):
+        self.report = report
+        self.run_id = run_id
+        yield
+
+
+pbar = KTSProgressBar()
