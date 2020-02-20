4079e1eb10679972241d187e153ae3c7d76375ff
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-02-20 11:40:09 +0300

    refactor if statement with new core.backend.util

--- kts/__init__.py
+++ kts/__init__.py
@@ -1,6 +1,7 @@
-import sys
+from kts.core.backend.util import in_cli, in_worker, in_pytest
 
-if not sys.argv[0].endswith('kts') and not sys.argv[0].endswith('default_worker.py'):
+
+if not in_cli() and not in_worker() and not in_pytest():
     from kts.api import *
     from kts.core.init import init
     init()
