93056da1103ba05eadda4c2c07ac1a155647b3b5
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-02-20 11:02:46 +0300

    add address_manager.timestamp check, ensure sending ObjectIDs inside Sync(), refactor checking if in worker, refactor suppressing stderr, refactor rm.supervise -> self.supervise

--- kts/core/feature_constructor/base.py
+++ kts/core/feature_constructor/base.py
@@ -1,6 +1,7 @@
 import time
 from abc import ABC
-from contextlib import redirect_stdout, contextmanager
+from contextlib import redirect_stdout, contextmanager, redirect_stderr
+from io import StringIO
 from typing import Tuple, Union, Dict
 
 import ray
@@ -9,12 +10,15 @@ from ray.experimental import signal as rs
 
 from kts.core.backend.address_manager import get_address_manager
 from kts.core.backend.io import RemoteTextIO, LocalTextIO
+from kts.core.backend.progress import pbar
 from kts.core.backend.signals import ResourceRequest, Sync
 from kts.core.backend.stats import Stats
+from kts.core.backend.util import in_worker
 from kts.core.backend.util import safe_put
 from kts.core.backend.worker import worker
-from kts.core.cache import RunID, AnyFrame
 from kts.core.frame import KTSFrame
+from kts.core.run_id import RunID
+from kts.core.types import AnyFrame
 
 
 class BaseFeatureConstructor(ABC):
@@ -23,11 +27,11 @@ class BaseFeatureConstructor(ABC):
     worker = None
 
     def request_resource(self, key, df):
-        if df._remote:
+        if in_worker():
             request_time = time.time()
             rs.send(ResourceRequest(key))
             address_manager = get_address_manager()
-            while not ray.get(address_manager.has.remote(key)): # TODO uncomment or ray.get(address_manager.timestamp.remote(key)) < request_time:
+            while not ray.get(address_manager.has.remote(key)) or ray.get(address_manager.timestamp.remote(key)) < request_time:
                 time.sleep(0.01)
             address = ray.get(address_manager.get.remote(key))
             resource = ray.get(address)
@@ -40,12 +44,15 @@ class BaseFeatureConstructor(ABC):
             return resource
 
     def sync(self, run_id, res_df, res_state, stats, df):
-        # TODO remove
-        # if run_id.fold == "preview":
-        #     return
         if not self.cache:
             res_df = None
-        if df.__meta__['remote']:
+        if in_worker():
+            if not isinstance(res_df, ObjectID):
+                res_df = ray.put(res_df)
+            if not isinstance(res_state, ObjectID):
+                res_state = ray.put(res_state)
+            if not isinstance(stats, ObjectID):
+                stats = ray.put(stats)
             rs.send(Sync(run_id, res_df, res_state, stats))
         else:
             rm = df.__meta__['run_manager']
@@ -54,14 +61,14 @@ class BaseFeatureConstructor(ABC):
     def __call__(self, df, ret=True):
         raise NotImplemented
 
-    def get_alias(self, df):
-        raise NotImplemented
-
     def remote_io(self):
         return redirect_stdout(RemoteTextIO())
 
     def local_io(self, report, run_id):
         return redirect_stdout(LocalTextIO(report, run_id))
+
+    def suppress_stderr(self):
+        return redirect_stderr(StringIO())
 
     @contextmanager
     def set_scope(self, kf: KTSFrame, scope: str):
@@ -70,52 +77,41 @@ class BaseFeatureConstructor(ABC):
         yield
         kf.__meta__['scope'] = tmp
 
-    # def setup_worker(self):
-    #     if self.worker is not None:
-    #         return
-
-    #     @ray.remote(num_return_vals=3)
-    #     def worker(*args, df: pd.DataFrame, meta: Dict):
-    #         kf = KTSFrame(df, meta=meta)
-    #         kf.__meta__['remote'] = True
-    #         had_state = bool(kf.state)
-    #         stats = Stats(df)
-    #         with stats:
-    #             with self.remote_io():
-    #                 res_kf = self.compute(*args, kf)
-    #         if had_state:
-    #             res_state = None
-    #         else:
-    #             res_state = res_kf._state
-    #         return res_kf, res_state, stats.data
-
-    #     self.worker = worker
-
     def local_worker(self, *args, kf: KTSFrame):
         run_id = RunID(kf._scope, kf._fold, kf.hash())
         had_state = bool(kf.state)
         stats = Stats(kf)
-        if kf._remote:
+        if in_worker():
+            report = None
             io = self.remote_io()
-        # else:
-        #     io = self.local_io(kf.__meta__['report'], run_id)
-        with stats:
-            # with io:
+        else:
+            report = kf.__meta__['report']
+            io = self.local_io(report, run_id)
+            report.update(run_id, 0, 1)
+        with stats, io, self.suppress_stderr(), pbar.local_mode(report, run_id):
             res_kf = self.compute(*args, kf)
         if had_state:
             res_state = None
         else:
             res_state = kf._state
+        if not in_worker():
+            report = kf.__meta__['report']
+            report.update(run_id, 1, 1)
         return res_kf, res_state, stats.data
 
     def schedule(self, *args, scope: str, kf: KTSFrame) -> Tuple[RunID, Union[ObjectID, AnyFrame], Union[ObjectID, Dict], Union[ObjectID, Dict]]:
         run_id = RunID(scope, kf._fold, kf.hash())
         with self.set_scope(kf, scope):
             if self.parallel:
-                # self.setup_worker()
                 meta = kf.__meta__
                 oid = safe_put(kf)
                 res_df, res_state, stats = worker.remote(self, *args, df=oid, meta=meta)
             else:
                 res_df, res_state, stats = self.local_worker(*args, kf=kf)
         return run_id, res_df, res_state, stats
+
+    def supervise(self, kf: KTSFrame):
+        if 'run_manager' in kf.__meta__:
+            rm = kf.__meta__['run_manager']
+            report = kf.__meta__['report']
+            rm.supervise(report=report)
