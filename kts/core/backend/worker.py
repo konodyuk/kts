4235a747ee4522cff009eeeb338f77fe0b27e3ce
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-02-20 10:55:59 +0300

    send pid, progress on start and progress on finish, refactor with-statement

--- kts/core/backend/worker.py
+++ kts/core/backend/worker.py
@@ -1,6 +1,4 @@
 import os
-from contextlib import redirect_stderr
-from io import StringIO
 from typing import Dict
 
 import pandas as pd
@@ -15,19 +13,23 @@ from kts.core.frame import KTSFrame
 
 @ray.remote(num_return_vals=3, max_retries=0)
 def worker(self, *args, df: pd.DataFrame, meta: Dict):
-    rs.send(ProgressSignal(0, 1, None, None))
-    rs.send(RunPID(os.getpid()))
+    assert 'run_manager' not in meta
+    assert 'report' not in meta
     kf = KTSFrame(df, meta=meta)
     kf.__meta__['remote'] = True
     had_state = bool(kf.state)
+
+    rs.send(ProgressSignal(0, 1, None, None, None))
+    rs.send(RunPID(os.getpid()))
+
     stats = Stats(df)
-    with stats:
-        with self.remote_io():
-            with redirect_stderr(StringIO()):
-                res_kf = self.compute(*args, kf)
+    with stats, self.remote_io(), self.suppress_stderr():
+        res_kf = self.compute(*args, kf)
+
     if had_state:
         res_state = None
     else:
         res_state = kf._state
-    rs.send(ProgressSignal(1, 1, None, None))
+
+    rs.send(ProgressSignal(1, 1, None, None, None))
     return res_kf, res_state, stats.data
