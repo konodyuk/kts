3c27192b717be6b37b8f7f611a0c74bd2fd1c1aa
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-02-21 19:01:34 +0300

    add runtime and static cache tests

--- kts/tests/test_backend.py
+++ kts/tests/test_backend.py
@@ -89,18 +89,58 @@ def test_deeply_nested_fc_run(ktsframe, run_manager, report, remote_1, remote_2,
     run_manager.merge_scheduled()
     assert all(res_frame == (ktsframe + 1) ** 2 + (ktsframe + 1) ** 4)
 
-@pytest.mark.skip
-def test_scheduler_cache(): # test_runtime_cache
+@pytest.mark.parametrize('remote_2,remote_3', list(product([True, False], repeat=2)) * 1)
+def test_scheduler_cache(ktsframe, run_manager, report, remote_2, remote_3, remote_1=True): # test_runtime_cache
     def func_1(df):
         return df + np.random.randint(1000)
-    return
-    res = run_manager.run([func_1 for _ in range(5)])
-    for df in res:
-        assert df == res[0]
+    func_1 = FeatureConstructor(func_1)
+    func_1.parallel = remote_1
+    def func_2(df):
+        return func_1(df)
+    func_2 = FeatureConstructor(func_2)
+    func_2.parallel = remote_2
+    def func_3(df):
+        return func_1(df)
+    func_3 = FeatureConstructor(func_3)
+    func_3.parallel = remote_3
+    res = run_manager.run([func_1, func_2, func_3], frame=ktsframe, ret=True, remote=True, report=report)
+    assert all(res['func_1'] == res['func_2'])
+    assert all(res['func_1'] == res['func_3'])
 
-@pytest.mark.skip
-def test_static_cache():
-    pass
+@pytest.mark.parametrize('remote_1', list(product([True, False], repeat=1)) * 1)
+def test_static_cache(ktsframe, run_manager, report, remote_1):
+    ktsframe.__meta__['fold'] = 'test_static_cache'
+    def func_1(df):
+        return df + np.random.randint(1000)
+    func_1 = FeatureConstructor(func_1)
+    func_1.parallel = remote_1
+    res_1 = run_manager.run([func_1], frame=ktsframe, ret=True, remote=True, report=report)['func_1']
+    run_manager.merge_scheduled()
+    assert len(run_manager.scheduled) == 0
+    res_2 = run_manager.run([func_1], frame=ktsframe, ret=True, remote=True, report=report)['func_1']
+    assert all(res_1 == res_2)
+
+@pytest.mark.parametrize('remote_1,remote_2,remote_3', list(product([True, False], repeat=3)) * 1)
+def test_deeply_nested_fc_scheduler_cache(ktsframe, run_manager, report, remote_1, remote_2, remote_3):
+    am.clear.remote()
+    def func_1(df):
+        return df + np.random.randint(1000)
+    func_1 = FeatureConstructor(func_1)
+    func_1.parallel = remote_1
+    def func_2(df):
+        return func_1(df) ** 2
+    func_2 = FeatureConstructor(func_2)
+    func_2.parallel = remote_2
+    def func_3(df):
+        return func_2(df) ** 2 + func_1(df)
+    func_3 = FeatureConstructor(func_3)
+    func_3.parallel = remote_3
+    assert len(run_manager.scheduled) == 0
+    res = run_manager.run([func_1, func_3], frame=ktsframe, ret=True, remote=remote_3, report=report)
+    res_1 = res['func_1']
+    res_3 = res['func_3']
+    run_manager.merge_scheduled()
+    assert all(res_3 == (res_1) ** 2 + (res_1) ** 4)
 
 @pytest.mark.skip
 def test_nested_concat():
