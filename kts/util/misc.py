8d6f2c24279a2545c665508cbc7db0e0be3429b3
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-03-01 13:52:48 +0300

    remove trash from /utils

--- kts/util/misc.py
+++ kts/util/misc.py
@@ -1,47 +1,5 @@
 import inspect
-import time
 from abc import ABCMeta
-from itertools import zip_longest
-
-import numpy as np
-
-
-def captcha():
-    np.random.seed(int(time.time()))
-    a, b = np.random.randint(5, 30, size=2)
-    c = int(input(f"{a} + {b} = "))
-    if a + b != c:
-        return False
-    return True
-
-def extract_signature(func, return_dict=False):
-    args = inspect.getfullargspec(func).args
-    defaults = inspect.getfullargspec(func).defaults
-    values = {
-        **inspect.currentframe().f_back.f_locals,
-        **inspect.currentframe().f_back.f_back.f_locals,
-    }
-    if defaults is None:
-        defaults = []
-    if args is None:
-        args = []
-    if return_dict:
-        return {arg: values[arg] for arg in args}
-    sources = []
-    for arg, default in list(zip_longest(args[::-1], defaults[::-1]))[::-1]:
-        if values[arg] != default:
-            if is_helper(values[arg]):
-                arg_repr = values[arg].__name__
-            else:
-                arg_repr = repr(values[arg])
-            sources.append(f"{arg}={arg_repr}")
-    return ", ".join(sources)
-
-
-def is_helper(func):
-    return (callable(func) and "__name__" in dir(func)
-            and "source" in dir(func) and isinstance(func.source, str)
-            and "def" in func.source)  # genius
 
 
 class BadSignatureException(Exception):
