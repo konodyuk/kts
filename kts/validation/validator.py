f1886721c3eb98c4b7d8441ce1ea5628f8ba9db7
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-03-02 15:11:41 +0300

    add reprs, fix oofs, fix repeated training exception

--- kts/validation/validator.py
+++ kts/validation/validator.py
@@ -1,15 +1,17 @@
 import numpy as np
 
+import kts.ui.components as ui
 from kts.core.feature_set import FeatureSet
 from kts.modelling.mixins import Model
 from kts.modelling.pipeline import CVPipeline
 from kts.settings import cfg
+from kts.ui.feature_computing_report import FeatureComputingReport
 from kts.util.misc import SourceMetaClass
 from kts.validation.experiment import Experiment, experiment_id
 from kts.validation.leaderboard import leaderboard_list
 
 
-class Validator(metaclass=SourceMetaClass):
+class Validator(ui.HTMLRepr, metaclass=SourceMetaClass):
     def __init__(self, splitter, metric):
         self.splitter = splitter
         self.metric = metric
@@ -23,6 +25,7 @@ class Validator(metaclass=SourceMetaClass):
         max_idx = 0
         for idx_train, idx_val in folds:
             max_idx = max(max_idx, np.max(idx_train), np.max(idx_val))
+        max_idx += 1
         res = np.zeros((max_idx,))
         weights = np.zeros((max_idx,))
         for pred, (idx_train, idx_val) in zip(predictions, folds):
@@ -35,12 +38,13 @@ class Validator(metaclass=SourceMetaClass):
 
     def score(self, model: Model, feature_set: FeatureSet, desc: str, leaderboard: str, **kwargs):
         cfg.preview_mode = False
-        if experiment_id(model, feature_set) in leaderboard:
+        if leaderboard in leaderboard_list and experiment_id(model, feature_set) in leaderboard_list[leaderboard]:
             raise UserWarning(f'Duplicate experiment: {experiment_id(model, feature_set)}')
         folds = self.create_folds(feature_set, self.splitter)
-        feature_set.compute()
+        folds = list(folds)
+        report = FeatureComputingReport()
         cv_feature_set = feature_set.split(folds)
-        cv_feature_set.compute()
+        cv_feature_set.compute(report=report)
         cv_pipeline = CVPipeline(cv_feature_set, model)
         cv_pipeline.fit(score_fun=self.evaluate, **kwargs)
         raw_oof = cv_pipeline.raw_oof
@@ -55,3 +59,23 @@ class Validator(metaclass=SourceMetaClass):
         leaderboard_list.register(experiment, leaderboard)
         cfg.preview_mode = True
         return {'score': experiment.score, 'id': experiment.id}
+
+    def _html_elements(self):
+        elements = [
+            ui.Annotation('splitter'),
+            ui.Code(repr(self.splitter)),
+            ui.Annotation('metric'),
+            ui.Code(self.metric.__name__)  # should also consider metrics defined as helpers
+        ]
+        return elements
+
+    @property
+    def html(self):
+        return ui.Column([ui.Title('validator')] + self._html_elements()).html
+
+    @property
+    def html_collapsible(self):
+        css_id = np.random.randint(1000000000)
+        elements = [ui.TitleWithCross('validator', css_id)]
+        elements += self._html_elements()
+        return ui.CollapsibleColumn(elements, ui.ThumbnailField('validator', css_id), css_id).html
