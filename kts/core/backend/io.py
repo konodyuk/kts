45881ab3e89d8a4d157c3c886547a894087c1183
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-03-02 13:52:44 +0300

    add optional run_id argument

--- kts/core/backend/io.py
+++ kts/core/backend/io.py
@@ -4,17 +4,22 @@ from ray.experimental import signal as rs
 
 
 class TextChunk(rs.Signal):
-    def __init__(self, timestamp, text):
+    def __init__(self, timestamp, text, run_id=None):
         self.timestamp = timestamp
         self.text = text
+        self.run_id = run_id
 
     def get_contents(self):
-        return {'timestamp': self.timestamp, 'text': self.text}
+        res = {'timestamp': self.timestamp, 'text': self.text}
+        if self.run_id is not None:
+            res['run_id'] = self.run_id
+        return res
 
 
 class RemoteTextIO:
-    def __init__(self):
+    def __init__(self, run_id=None):
         self.buf = ""
+        self.run_id = run_id
 
     def write(self, b):
         self.buf += b
@@ -23,7 +28,7 @@ class RemoteTextIO:
 
     def flush(self):
         if self.buf:
-            rs.send(TextChunk(time.time(), self.buf))
+            rs.send(TextChunk(time.time(), self.buf, self.run_id))
         self.buf = ""
 
 
