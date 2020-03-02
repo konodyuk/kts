3c2f32f663b69ad37fc86057117eb365e70bf2b7
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-03-02 15:13:27 +0300

    add HTMLReprMixin, add TrackingMixin

--- kts/modelling/mixins.py
+++ kts/modelling/mixins.py
@@ -1,9 +1,12 @@
+import numpy as np
+
+import kts.ui.components as ui
 from kts.util.hashing import hash_dict, hash_str
 
 
-class NamingMixin:
+class TrackingMixin:
     @property
-    def name(self):
+    def params(self):
         try:
             tracked_params = self.tracked_params
         except:
@@ -12,19 +15,24 @@ class NamingMixin:
             key: self.get_params()[key]
             for key in tracked_params if key in self.get_params()
         }
+        return params
 
+
+class NamingMixin(TrackingMixin):
+    @property
+    def name(self):
         name = self.__class__.__name__
-        name += hash_dict(params, 3)
+        name += hash_dict(self.params, 3)
         if 'class_source' in dir(self):
             name += hash_str(self.class_source, 2)
         return name
 
 
-class SourceMixin:
+class SourceMixin(TrackingMixin):
     @property
     def source(self):
         args = []
-        for key, value in self.get_params().items():
+        for key, value in self.params.items():
             args.append(f"{key}={repr(value)}")
         res = ", ".join(args)
         return f"{self.__class__.__name__}({res})"
@@ -54,7 +62,39 @@ class ProgressMixin:
         return 1
 
 
-class Model(NamingMixin, SourceMixin, PreprocessingMixin, ProgressMixin):
+class HTMLReprMixin(ui.HTMLRepr, TrackingMixin):
+    def _html_elements(self):
+        from kts.modelling.custom_model import CustomModel
+        elements = [
+            ui.Annotation('name'),
+            ui.Field(self.name),
+            ui.Annotation('model'),
+            ui.Field(self.__class__.__name__),
+            ui.Annotation('params'),
+            ui.Field(str(self.params)),
+            ui.Annotation('source'),
+            ui.Code(self.source)
+        ]
+        if isinstance(self, CustomModel):
+            elements += [
+                ui.Annotation('custom model class source'),
+                ui.Code(self.class_source)
+            ]
+        return elements
+
+    @property
+    def html(self):
+        return ui.Column([ui.Title('model')] + self._html_elements()).html
+
+    @property
+    def html_collapsible(self):
+        css_id = np.random.randint(1000000000)
+        elements = [ui.TitleWithCross('model', css_id)]
+        elements += self._html_elements()
+        return ui.CollapsibleColumn(elements, ui.ThumbnailField('model', css_id), css_id).html
+
+
+class Model(NamingMixin, SourceMixin, PreprocessingMixin, ProgressMixin, HTMLReprMixin):
     pass
 
 
