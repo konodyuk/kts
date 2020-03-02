7d5a502dca0dbbfdc7bcd031a01b10947784eccc
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-03-02 14:27:06 +0300

    fix reprs, show relative time of an experiment instead of absolute

--- kts/ui/leaderboard.py
+++ kts/ui/leaderboard.py
@@ -1,21 +1,21 @@
 from kts.ui.components import HTMLRepr, Column, Field, Title, ThumbnailField, Raw
 from kts.util.formatting import format_value
-
+import time
 
 class Leaderboard(HTMLRepr):
     """Needs refactoring, very sketchy"""
     def __init__(self, experiments):
         self.experiments = experiments
-        self.col_widths = [1, 6, 5, 10, 6, 4, 8]
+        self.col_widths = [1, 6, 5, 10, 6, 8, 8]
         self.col_names = ['#', 'id', 'score', 'model', '# features', "date", "took"]
         self.data = [
             (
                 i,
                 e.id,
                 format_value(e.score),
-                e.model.__class__,
+                e.model_class,
                 e.n_features,
-                format_value(e.date, time=True),
+                format_value(time.time() - e.date, time=True) + ' ago',
                 format_value(e.took, time=True)
             )
             for i, e in enumerate(experiments)
@@ -37,10 +37,10 @@ class Leaderboard(HTMLRepr):
         for i in range(1, len(self.col_widths)):
             head_cells.append(Field(self.col_names[i], **self.head_style(self.col_widths[i])))
 
-        rows = [[Field(self.data[i][j],  **self.cell_style(col_widths[j]))
+        rows = [[Field(self.data[i][j],  **self.cell_style(self.col_widths[j]))
             for j in range(len(self.data[0]))
         ] for i in range(len(self.data))]
-        rows = [Raw(e.html_collapsable(ThumbnailField(self.concat(rows[i]), cssid=-1, first=False), border=True)) for i, e in enumerate(self.experiments)]
+        rows = [Raw(e.html_collapsible(ThumbnailField(self.concat(rows[i]), css_id=-1, first=False), border=True)) for i, e in enumerate(self.experiments)]
 
         res = Column([Title('leaderboard'), Field(self.concat(head_cells), bg=False, bold=False, style="padding-bottom: 0px; margin: 0px 2px 0px 2px;")] + rows)
         return res.html
