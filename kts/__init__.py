c25b70ff85f59ff3931a5949d97f354b092b1a5a
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-03-09 18:16:04 +0300

    add kts.save, kts.load, kts.rm, kts.ls

--- kts/__init__.py
+++ kts/__init__.py
@@ -4,12 +4,13 @@ from kts.core.backend.util import in_cli, in_worker, in_pytest
 if not in_cli() and not in_worker() and not in_pytest():
     from kts.api.decorators import preview, feature, helper, generic
     from kts.core.feature_set import FeatureSet
-    from kts.core.lists import feature_list, helper_list
+    from kts.core.lists import feature_list as features, helper_list as helpers
     from kts.core.frame import KTSFrame
     from kts.modelling.custom_model import CustomModel
     from kts.validation.leaderboard import leaderboard, leaderboard_list
     from kts.validation.validator import Validator
     from kts.ui.settings import set_highlighting, set_theme, set_animation
+    from kts.core.cache import save, load, rm, ls
     import kts.stl as stl
 
     lb = leaderboard
